c7cbcca7df8185ffeaecb239f62885c6625347b2
Pablo Pareja Tobes <pablo.pareja@vizzuality.com>
2020-08-07 13:49:12 +0200

    some progress on the clean-up

--- modules/index.js
+++ modules/index.js
@@ -1,14 +1,8 @@
 import { handleModule } from 'redux-tools';
 
-import blogModule from './blog';
-import dashboardsModule from './dashboards';
-import partnersModule from './partners';
 import staticPagesModules from './static-pages';
 
 export default {
-  blog: handleModule(blogModule),
-  dashboards: handleModule(dashboardsModule),
-  partners: handleModule(partnersModule),
   staticPages: handleModule(staticPagesModules)
 };
 
--- redactions/index.js
+++ redactions/index.js
@@ -6,16 +6,11 @@ export { default as modal } from './modal';
 export { default as user } from './user';
 export { default as routes } from './routes';
 export { default as page } from './page';
-export { default as insights } from './insights';
-export { default as clientDashboards } from './dashboards';
 
 // Admin
 export { default as datasets } from './admin/datasets';
-export { default as adminDashboards } from './admin/dashboards';
 export { default as pages } from './admin/pages';
-export { default as tools } from './admin/tools';
 export { default as sources } from './admin/sources';
-export { default as faqs } from './admin/faqs';
 
 // Toastr
 export { reducer as toastr } from 'react-redux-toastr';
