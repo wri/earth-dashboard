e4a96e9da6a2567b6fdd90f549dd597bf09c5287
Pablo Pareja Tobes <pablo.pareja@vizzuality.com>
2020-09-08 10:32:39 +0200

    first implementation of list type widget

---
+++ components/widgets/list/component.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import { toastr } from 'react-redux-toastr';
+
+// components
+import Spinner from 'components/ui/Spinner';
+
+// styles
+import './styles.scss';
+
+function ListWidget(props) {
+  const { widget } = props;
+  const widgetConfig = widget && widget.widgetConfig;
+  const listWidgetConfig = widgetConfig && widgetConfig.listWidgetConfig;
+  const { heading, query, numbers, bullets, format } = listWidgetConfig || {};
+  const [loading, setLoading] = useState(true);
+  const [listData, setListdata] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetch(query)
+      .then(resp => resp.json())
+      .then((response) => {
+        setListdata(response.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        toastr.error(`There was an error loading the widget ${widget.name} query: ${err}`);
+      });
+  }, [query, widget]);
+
+  const ListTag = numbers ? 'ol' : 'ul';
+
+  return (
+    <div className="c-list-widget">
+      <Spinner isLoading={loading} className="-relative -light" />
+      <div className="list-heading">
+        {heading}
+      </div>
+      <ListTag>
+        {listData.map(elem =>
+            <li>
+                {elem.key}: {elem.value}
+            </li>
+        )}
+      </ListTag>
+    </div>
+  );
+}
+
+ListWidget.propTypes = { widget: PropTypes.object.isRequired };
+
+export default ListWidget;
