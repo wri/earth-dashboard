d44bcbe03bfccb1280e7cc58c6a5a2ff4f01ca35
Pablo Pareja Tobes <pablo.pareja@vizzuality.com>
2020-10-21 15:22:27 +0200

    add framer effect when opening nav menu

--- layout/header/component.js
+++ layout/header/component.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Particles from 'react-particles-js';
 import Link from 'next/link';
 import classnames from 'classnames';
+import { motion } from "framer-motion"
 
 // utils
 import { PARTICLES_DEFINITION } from 'utils/particles';
@@ -25,51 +26,53 @@ function Header() {
       >
 
       </div>
-      {isOpen &&
-        <div className={styles['menu-container']}>
-          <Particles
-            className={styles.particles}
-            params={PARTICLES_DEFINITION}
-          />
-          <div className={styles['data-containers']}>
-            <div className={styles['left-container']}>
-              <ul>
-                <li>About</li>
-                <li>Share</li>
-              </ul>
-            </div>
-            <div className={styles['right-container']}>
-              <ul>
-                {HEADER_TOPICS_DATA.map(topicData =>
-                  <li className={styles['topic-container']}>
-                    <div className={styles['topic-title']}>
-                      <span style={{ borderBottom: `solid 2px ${topicData.color}` }}>
-                        {topicData.label}
-                      </span>
-                    </div>
-                    <ul className={styles['topic-link-list']}>
-                      {topicData.links.map(linkData =>
-                        <li className={styles['topic-link']}>
-                          <Link href={`${topicData.mainLink}${linkData.link}`}>
-                            <a 
-                              className={classnames({ [styles['-highlighted-link']]: linkData.highlight })}
-                              style={linkData.highlight ? {
-                                borderBottom: `solid 2px ${topicData.color}`
-                              } : {}}
-                            >
+      <motion.div animate={{ opacity: isOpen ? 1 : 0 }}>
+        {isOpen &&
+          <div className={styles['menu-container']}>
+            <Particles
+              className={styles.particles}
+              params={PARTICLES_DEFINITION}
+            />
+            <div className={styles['data-containers']}>
+              <div className={styles['left-container']}>
+                <ul>
+                  <li>About</li>
+                  <li>Share</li>
+                </ul>
+              </div>
+              <div className={styles['right-container']}>
+                <ul>
+                  {HEADER_TOPICS_DATA.map(topicData =>
+                    <li className={styles['topic-container']}>
+                      <div className={styles['topic-title']}>
+                        <span style={{ borderBottom: `solid 2px ${topicData.color}` }}>
+                          {topicData.label}
+                        </span>
+                      </div>
+                      <ul className={styles['topic-link-list']}>
+                        {topicData.links.map(linkData =>
+                          <li className={styles['topic-link']}>
+                            <Link href={`${topicData.mainLink}${linkData.link}`}>
+                              <a
+                                className={classnames({ [styles['-highlighted-link']]: linkData.highlight })}
+                                style={linkData.highlight ? {
+                                  borderBottom: `solid 2px ${topicData.color}`
+                                } : {}}
+                              >
                                 {linkData.label}
-                            </a>
-                          </Link>
-                        </li>
-                      )}
-                    </ul>
-                  </li>
-                )}
-              </ul>
+                              </a>
+                            </Link>
+                          </li>
+                        )}
+                      </ul>
+                    </li>
+                  )}
+                </ul>
+              </div>
             </div>
           </div>
-        </div>
-      }
+        }
+      </motion.div>
     </header>
   );
 }
