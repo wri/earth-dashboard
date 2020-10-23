3f2743169ed4b0686f6b2c5825c65655391cae4c
Pablo Pareja Tobes <pablo.pareja@vizzuality.com>
2020-10-23 09:13:50 +0200

    add logo to footer

--- layout/app/topic/data/footer/component.js
+++ layout/app/topic/data/footer/component.js
@@ -8,28 +8,36 @@ function Footer() {
     return (
         <div className={styles['c-topic-data-footer']}>
             <div className={styles['main-container']}>
-                <div>LOGO</div>
+                <img src="/static/images/logo-light.svg" />
                 <div className={styles['topic-links']}>
-                    <Link href="/climate/data">
-                        <a className={styles['climate-link']}>
-                            CLIMATE
-                        </a>
-                    </Link>
-                    <Link href="/forests/data">
-                        <a className={styles['forests-link']}>
-                            FORESTS
-                        </a>
-                    </Link>
-                    <Link href="/freshwater/data">
-                        <a className={styles['freshwater-link']}>
-                            FRESHWATER
-                        </a>
-                    </Link>
-                    <Link href="/oceans/data">
-                        <a className={styles['oceans-link']}>
-                            OCEANS
-                        </a>
-                    </Link>
+                    <div>
+                        <Link href="/climate/data">
+                            <a className={styles['climate-link']}>
+                                CLIMATE
+                            </a>
+                        </Link>
+                    </div>
+                    <div>
+                        <Link href="/forests/data">
+                            <a className={styles['forests-link']}>
+                                FORESTS
+                            </a>
+                        </Link>
+                    </div>
+                    <div>
+                        <Link href="/freshwater/data">
+                            <a className={styles['freshwater-link']}>
+                                FRESHWATER
+                            </a>
+                        </Link>
+                    </div>
+                    <div>
+                        <Link href="/oceans/data">
+                            <a className={styles['oceans-link']}>
+                                OCEANS
+                            </a>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
