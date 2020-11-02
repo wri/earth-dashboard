c3cc46950ec39e04c39a02441384d5683c3d6138
Tiago Garcia <tiagojsag@gmail.com>
2020-11-02 14:56:30 +0100

    Restore production NODE_ENV

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@ server 'ec2-100-26-157-165.compute-1.amazonaws.com', user: 'ubuntu', roles: %w{w
 set :ssh_options, forward_agent: true
 
 set :default_env, {
-  'NODE_ENV' => 'development',
+  'NODE_ENV' => 'production',
   'WRI_API_URL' => 'https://api.resourcewatch.org/v1',
   'CONTROL_TOWER_URL' => 'https://api.resourcewatch.org',
   'CALLBACK_URL' => 'https://resourcewatch.org/auth',
