a66a00908f44bc0a7be8198a456b57351bb90f3a
Tiago Garcia <tiagojsag@gmail.com>
2020-11-02 13:18:34 +0100

    Add support for custom port env var

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const path = require('path');
 
 const nextPath = path.join(__dirname, 'node_modules', '.bin', 'next');
 
+const port = process.env.PORT || '3000';
 process.argv.length = 1;
-process.argv.push(nextPath, 'start');
+process.argv.push(nextPath, 'start', port);
 
 // eslint-disable-next-line import/no-dynamic-require
 require(nextPath);
