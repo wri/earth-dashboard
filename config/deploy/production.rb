bf75da7443bd7aa878461ea53d902e57e14f221e
Tiago Garcia <tiagojsag@gmail.com>
2020-11-02 13:15:46 +0100

    Temporarily set production node_env to development; add APPLICATIONS env var to capistrano env

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,8 +3,9 @@ server 'ec2-100-26-157-165.compute-1.amazonaws.com', user: 'ubuntu', roles: %w{w
 set :ssh_options, forward_agent: true
 
 set :default_env, {
-  'NODE_ENV' => 'production',
+  'NODE_ENV' => 'development',
   'WRI_API_URL' => 'https://api.resourcewatch.org/v1',
   'CONTROL_TOWER_URL' => 'https://api.resourcewatch.org',
-  'CALLBACK_URL' => 'https://resourcewatch.org/auth'
+  'CALLBACK_URL' => 'https://resourcewatch.org/auth',
+  'APPLICATIONS' => 'earthhq'
 }
