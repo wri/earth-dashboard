ab053b61ee3c5cd9758d8ba91d08ae5f60d6835f
Tiago Garcia <tiagojsag@gmail.com>
2020-11-02 13:30:51 +0100

    Remove support for custom port env var

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const path = require('path');
 
 const nextPath = path.join(__dirname, 'node_modules', '.bin', 'next');
 
-const port = process.env.PORT || '3000';
 process.argv.length = 1;
-process.argv.push(nextPath, 'start', port);
+process.argv.push(nextPath, 'start');
 
 // eslint-disable-next-line import/no-dynamic-require
 require(nextPath);
