92312ccfb7da04601dc15d3c58eef6a2a5fe66f8
Pablo Pareja Tobes <pablo.pareja@vizzuality.com>
2020-11-05 14:14:19 +0100

    add missing import

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Document, { Html, Main, NextScript, Head } from 'next/document';
+import { basicAuthMiddleware } from 'nextjs-basic-auth-middleware';
 
 class MyDocument extends Document {
   render() {
@@ -73,9 +74,9 @@ class MyDocument extends Document {
 }
 
 MyDocument.getInitialProps = async (ctx) => {
+  await basicAuthMiddleware(ctx.req, ctx.res);
   const initialProps = await Document.getInitialProps(ctx);
-  await basicAuthMiddleware(req, res);
   return { ...initialProps };
-}
+};
 
 export default MyDocument;
