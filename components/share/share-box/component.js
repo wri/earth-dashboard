0c9224eeec3ed80fb1cd40e2acc840bfc1deb1db
Pablo Pareja Tobes <pablo.pareja@vizzuality.com>
2020-11-12 11:42:12 +0100

    add copy text functionality

--- components/share/share-box/component.js
+++ components/share/share-box/component.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classnames from 'classnames';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 // utils
 import {
@@ -17,33 +18,53 @@ import {
 import styles from './share-box.module.scss';
 
 function ShareBox({ url, topic }) {
+    const isServer = typeof window === 'undefined';
+
     return (
         <div className={styles['c-share-box']}>
             <div className={styles.['url-container']}>
-                <span>{url}</span>
+                <input
+                    type="text"
+                    value={url}
+                    disabled
+                />
             </div>
             <div className={styles['share-buttons']}>
-                <div className={styles['share-button']}>
-                    <img src={getEmailIconPerTopic(topic)} />
-                </div>
-                <div className={styles['share-button']}>
-                    <img src={getFacebookIconPerTopic(topic)} />
-                </div>
-                <div className={styles['share-button']}>
-                    <img src={getTwitterIconPerTopic(topic)} />
-                </div>
+                <a href={`mailto:?subject=Shared from Earth Dashboard&body= I thought you'd be interested in what I found on Earth Dashboard: ${url}`}>
+                    <div className={styles['share-button']}>
+                        <img src={getEmailIconPerTopic(topic)} />
+                    </div>
+                </a>
+                <a href={`http://www.facebook.com/sharer/sharer.php?u=${url}`}
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    <div className={styles['share-button']}>
+                        <img src={getFacebookIconPerTopic(topic)} />
+                    </div>
+                </a>
+                <a href={`https://twitter.com/share?url=${url}&text=${encodeURIComponent(
+                    isServer ? '' : document.title
+                )}`}
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    <div className={styles['share-button']}>
+                        <img src={getTwitterIconPerTopic(topic)} />
+                    </div>
+                </a>
             </div>
-            <button
-                className={classnames({
-                    '-forests': topic === FORESTS,
-                    '-oceans': topic === OCEANS,
-                    '-climate': topic === CLIMATE,
-                    '-freshwater': topic === FRESHWATER,
-                    [styles['copy-link-button']]: true
-                })}
-            >
-                Copy link
-            </button>
+            <CopyToClipboard text={url}>
+                <button
+                    className={classnames({
+                        '-forests': topic === FORESTS,
+                        '-oceans': topic === OCEANS,
+                        '-climate': topic === CLIMATE,
+                        '-freshwater': topic === FRESHWATER,
+                        [styles['copy-link-button']]: true
+                    })}
+                >
+                    Copy link
+                </button>
+            </CopyToClipboard>
         </div>
     );
 }
