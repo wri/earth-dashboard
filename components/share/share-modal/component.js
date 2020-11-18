2b207320931d9babdb62048f1aba098a51083631
Pablo Pareja Tobes <pablo.pareja@vizzuality.com>
2020-11-18 10:51:32 +0100

    first mobile version for share modal

--- components/share/share-modal/component.js
+++ components/share/share-modal/component.js
@@ -1,23 +1,41 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 // components
 import ShareBox from '../share-box';
 
 // utils
 import { getColorByTopic } from 'utils/topics';
+import { Mobile, Desktop, MediaContextProvider } from 'utils/responsive';
 
 // styles
 import styles from './share-modal.module.scss';
 
 function ShareModal({ topic, onClose }) {
 
-    return (
-        <div className={styles['c-share-modal']}>
+    const getContent = (mobile=false) =>
+        <div className={classnames({
+            [styles['main-container']]: true,
+            [styles['-mobile']]: mobile,
+            [styles['-desktop']]: !mobile
+        })}>
             <h2 style={{ color: getColorByTopic(topic) }}>Share</h2>
             <h5>Public url to share</h5>
             <ShareBox url="to be defined..." topic={topic} />
             <button className={styles['close-button']} onClick={() => onClose()} />
+        </div>;
+
+    return (
+        <div className={styles['c-share-modal']}>
+            <MediaContextProvider>
+                <Desktop>
+                    {getContent(false)}
+                </Desktop>
+                <Mobile>
+                    {getContent(true)}
+                </Mobile>
+            </MediaContextProvider>
         </div>
     );
 }
