8f0a7e86f993b9064f9d773748741b4a7147bdd9
Pablo Pareja Tobes <pablo.pareja@vizzuality.com>
2020-11-20 14:32:29 +0100

    get divider working

--- layout/app/topic/data/freshwater/scrolly-telling/water-drop/arrow/component.js
+++ layout/app/topic/data/freshwater/scrolly-telling/water-drop/arrow/component.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 // styles
 import styles from './arrow.module.scss';
 
 function Arrow({ direction }) {
+    const overflowShown = direction === 'up-and-down';
 
-    return(
-        <div className={styles['c-arrow']}>
+    return (
+        <div className={classnames({
+            [styles['c-arrow']]: true,
+            [styles['-overflow-shown']]: overflowShown
+        })}>
             {direction === 'up' &&
                 <div className={styles['arrow-up']}>
                     <img className={styles['first-arrow-up']} src="/static/images/scrolly-telling/freshwater/arrow_up.svg" />
@@ -22,7 +27,10 @@ function Arrow({ direction }) {
             }
             {direction === 'up-and-down' &&
                 <div className={styles['arrow-up-and-down']}>
-                    <img className={styles['arrow']} src="/static/images/scrolly-telling/freshwater/arrow_up_and_down.svg" />
+                    <div className={styles['container']}>
+                        <img className={styles['arrow']} src="/static/images/scrolly-telling/freshwater/arrow_up_and_down.svg" />
+                        <img className={styles['divider']} src="/static/images/scrolly-telling/freshwater/divider.svg" />
+                    </div>
                 </div>
             }
         </div>
