e4a9a8bd6deb5f5d916ab7c7c62ec83bb07f3406
Pablo Pareja Tobes <pablo.pareja@vizzuality.com>
2020-11-24 15:38:02 +0100

    update freshwater text cards

--- layout/app/topic/data/freshwater/scrolly-telling/constants.js
+++ layout/app/topic/data/freshwater/scrolly-telling/constants.js
@@ -18,17 +18,18 @@ export const FRESHWATER_STEPS = [
     {
         index: 0,
         textPanel: {
-            text: <p>Every region in the world has a <strong>yearly renewable water budget</strong>, available to the people, plants and animals living on it. It is represented by that big drop of water.</p>
+            text: <p>
+                <strong>Every region in the world has a yearly renewable water budget</strong>, available to people, plants and animals living on it. It is represented by this big water droplet. 
+                </p>
         }
     },
     {
         index: 1,
         textPanel: {
             text: <>
-            <h6>FRESHWATER SUPPLIES</h6>
-            <p>On the <strong>supply side</strong>, that water comes in the form of rainfall, rivers, lakes and other groundwater sources.</p>
-                <p>That’s the amount of water that the region can naturally replenish <strong>every year</strong>. </p>
-                <p className={styles['photo-subtitle']}>Photo by Kevin Dooley on Flickr <a href="https://creativecommons.org/licenses/by/2.0/" target="_blank">(cc-by-2.0)</a></p>
+                    <h6 className="freshwater">FRESHWATER SUPPLIES</h6>
+                    <p><strong>On the supply side</strong>, that water comes in the form of rainfall, rivers, lakes and other sources. That’s the amount of water that the region can naturally replenish every year.</p>
+                    <p className={styles['photo-subtitle']}>Photo by Kevin Dooley on Flickr <a href="https://creativecommons.org/licenses/by/2.0/" target="_blank">(cc-by-2.0)</a></p>
                 </>,
                 imageHeader: '/static/images/scrolly-telling/freshwater/freshwater-supplies.jpg'
         },
@@ -38,7 +39,7 @@ export const FRESHWATER_STEPS = [
         index: 2,
         textPanel: {
             text: <>
-                <h6>FRESHWATER WITHDRAWALS</h6>
+                <h6 className="freshwater">FRESHWATER WITHDRAWALS</h6>
                 <p>
                     From that budget, <strong>people withdraw water</strong> to use it in their homes, to 
                     run factories, to irrigate fields and for the livestock they raise.
@@ -52,11 +53,13 @@ export const FRESHWATER_STEPS = [
     {
         index: 3,
         textPanel: {
-            text: <>
-                <p>Ideally, no region should withdraw more water that it can naturally replenish.</p>
-                <p><strong>Baseline water stress</strong> measures the ratio of total water withdrawals to available renewable surface and groundwater supplies.</p>
-                <p>It is expressed as a percentage.  These risk scores range from low water stress ({'<'}10%) to extremely high water stress ({'>'}80%)</p>
-            </>
+            text:
+                <p>
+                    Ideally, no region should withdraw more water than it can naturally replenish. <strong>Baseline water stress</strong> measures
+                     the ratio of total water withdrawals to available renewable surface and groundwater 
+                    supplies. It is expressed as a percentage. <a className="external-link -freshwater" href="https://resourcewatch.org/data/explore/wat050-Aqueduct-Baseline-Water-Stress" target="_blank">These risk scores range from low water stress
+                     (&lt;10%) to extremely high water stress (&gt;80%)</a>.
+                </p>
         },
         dropAnimation: BASELINE_WATER_STRESS_ANIMATION
     },
@@ -64,13 +67,13 @@ export const FRESHWATER_STEPS = [
         index: 4,
         textPanel: {
             text: <>
-                <h6>WATER STRESS RISK</h6>
-                <p>If a region overspends its budget, we say it has a <strong>high water stress risk</strong>. Among the consequences are:</p>
+                <h6 className="freshwater">WATER STRESS RISK</h6>
+                <p>If a region overspends its budget, we say it has a high water stress risk. Among the consequences are:</p>
                 <ul>
                     <li>It has to <strong>import water</strong> from other regions</li>
-                    <li>It has to <strong>use non-renewable</strong> groundwater sources, which run the risk of depletion</li>
-                    <li><strong>Build reservoir infrastructures</strong> with high ecological impact</li>
-                    <li><strong>Be less resilient to natural hazards</strong> such as droughts, desertification, etc.</li>
+                    <li>It has to <strong>use non-renewable groundwater sources</strong>, which run the risk of depletion</li>
+                    <li>It has to <strong>build reservoir infrastructure</strong> with high ecological impact</li>
+                    <li>It will be <strong>less resilient to natural hazards</strong> such as droughts, desertification, etc.</li>
                 </ul>
                 <p className={styles['photo-subtitle']}>Photo by John Gibbons on Unsplash</p>
             </>,
@@ -83,7 +86,7 @@ export const FRESHWATER_STEPS = [
         index: 5,
         textPanel: {
             text: <p>
-                Let's explore some regions in the world with extremely high <span style={{ color: HIGHLIGHT_ORANGE_COLOR }}><strong>water stress risk</strong></span> ({'>'}80%)
+                Let’s explore some regions of the work with <strong>extremely high water stress risk</strong> (&gt;80%)
             </p>
         },
         dropAnimation: WATER_STRESS_RISK_FINAL
@@ -96,9 +99,9 @@ export const FRESHWATER_STEPS_WORLDMAP = [
         textPanel: {
             text:
                 <>
-                    <h6>Middle East and North Africa (MENA)</h6>
+                    <h6 className="freshwater">Middle East and North Africa (MENA)</h6>
                     <p>
-                        This region is home to 12 of the world’s 17 most water stressed countries.  Climate change will make water still scarcer by 2050 causing economic losses ranging from six to 14 percent of GDP.
+                        <strong>This region is home to 12 of the world’s 17 most water stressed countries</strong>. Climate change will make water still scarcer by 2050 causing economic losses ranging from six to 14 percent of GDP.
                      </p>
                 </>
         }
@@ -108,9 +111,11 @@ export const FRESHWATER_STEPS_WORLDMAP = [
         textPanel: {
             text:
                 <>
-                    <h6>Chennai, India</h6>
+                    <h6 className="freshwater">Chennai, India</h6>
                     <p>
-                        Reservoirs in <a className="external-link -freshwater" target="_blank" href="https://www.wri.org/blog/2019/06/how-does-flood-prone-city-run-out-water-inside-chennai-day-zero-crisis">Chennai</a> nearly ran dry, forcing the city’s 10 million residents to buy water or draw it from wells, further draining aquifers.  India’s water sources are being depleted and researchers say the nation faces the worst water crisis in its history.
+                    <a className="external-link -freshwater" href="https://www.wri.org/blog/2019/06/how-does-flood-prone-city-run-out-water-inside-chennai-day-zero-crisis" target="_blank">Reservoirs in Chennai nearly ran dry</a>, forcing the city’s 10 million residents to buy water or draw it 
+                    from wells, further draining aquifers.  India’s water sources are being depleted and researchers 
+                    say <strong>the nation faces the worst water crisis in its history</strong>. 
                      </p>
                 </>
         }
@@ -120,9 +125,11 @@ export const FRESHWATER_STEPS_WORLDMAP = [
         textPanel: {
             text:
                 <>
-                    <h6>Cape Town, South Africa</h6>
+                    <h6 className="freshwater">Cape Town, South Africa</h6>
                     <p>
-                        Three years of drought left Cape Town facing the spectre of “Day Zero,” the day South Africa’s second largest city government would need to <a className="external-link -freshwater" target="_blank" href="https://www.nytimes.com/2018/01/30/world/africa/cape-town-day-zero.html">shut off</a> water taps for most homes and businesses. Aggressive conservation measures and rain spared the city.                     
+                    <strong>Three years of drought</strong> left Cape Town facing the spectre of “Day Zero,” the day South 
+                    Africa’s second largest city government would need to <a className="external-link -freshwater" href="https://www.nytimes.com/2018/01/30/world/africa/cape-town-day-zero.html" target="_blank">shut off water taps</a> for 
+                    most homes and businesses. Aggressive conservation measures and rain spared the city.
                     </p>
                 </>
         }
@@ -132,9 +139,11 @@ export const FRESHWATER_STEPS_WORLDMAP = [
         textPanel: {
             text:
                 <>
-                    <h6>Rome, Italy</h6>
+                    <h6 className="freshwater">Rome, Italy</h6>
                     <p>
-                        Rome endured months with hardly any rain - a drop of 70% from previous years.  The city resorted to cutting water pressure during the night on peak days.  Some in tall buildings lost service.  The Vatican turned off its 100 fountains.                    
+                        Rome endured months with hardly any rain - <strong>a drop of 70%</strong> from previous years. 
+                        The city resorted to cutting water pressure during the night on peak days. Some in 
+                        tall buildings lost service. The Vatican turned off its 100 fountains.  
                     </p>
                 </>
         }
