825f45442ef780bc7c3dd4ca94920a2a60dc7dac
Pablo Pareja Tobes <pablo.pareja@vizzuality.com>
2020-11-24 15:54:03 +0100

    freshwater challenge to our commons narrative update

--- layout/app/topic/data/freshwater/constants.js
+++ layout/app/topic/data/freshwater/constants.js
@@ -6,32 +6,32 @@ export const FRESHWATER_HEADLINE_SECTION_DATA = {
 export const FRESHWATER_CHALLENGE_SECTION_DATA = {
     paragraphs: [
         <p>
-            Almost half of humanity (3.6 billion people) lives in areas likely to run short 
-            of water at least one month a year. By withdrawing more water than the land can naturally 
-            replenish, we have created humanitarian crises and security risks <a className="external-link -freshwater" href="https://www.forbes.com/sites/paullaudicina/2018/06/07/water-day-zero-coming-to-a-city-near-you/#5df9cd5c1738" target="_blank">around the world</a>. 
-            From Bangalore to Beijing, <a className="external-link -freshwater" href="https://www.cnn.com/2018/03/09/africa/cape-town-day-zero-crisis-intl/index.html" target="_blank">Cape Town</a> to
-             California and Melbourne to Mexico City water shortages and even rationing are commonplace. 
-            Nature is suffering, too, with freshwater species declining faster than their ocean or forest
-             cousins—<a href="https://livingplanet.panda.org/en-US/freshwater" target="_blank">by 84%, on average</a>, since 1970. 
+           Almost half of humanity (3.6 billion people) lives in areas likely to run short of water at least one 
+           month a year. By withdrawing more water than the land can naturally replenish, we have created 
+           humanitarian crises and security risks <a className="external-link -freshwater" href="https://www.forbes.com/sites/paullaudicina/2018/06/07/water-day-zero-coming-to-a-city-near-you/#5df9cd5c1738" target="_blank">around the world</a>. From Bangalore 
+           to Beijing, <a className="external-link -freshwater" href="https://www.cnn.com/2018/03/09/africa/cape-town-day-zero-crisis-intl/index.html" target="_blank">Cape Town</a> to 
+           California and Melbourne to Mexico City water shortages and even rationing are commonplace. Nature 
+           is suffering, too, with freshwater species declining faster than their ocean or forest 
+           cousins—<a className="external-link -freshwater" href="https://livingplanet.panda.org/en-US/freshwater" target="_blank">by 84%, on average</a>, since 1970. 
         </p>,
         <p>
-            Food demand is driving the water deficit, with agriculture responsible for 
-            <a className="external-link -freshwater" href="https://blogs.worldbank.org/opendata/chart-globally-70-freshwater-used-agriculture#:~:text=In%20most%20regions%20of%20the,percent%20increase%20in%20water%20withdrawals." target="_blank">70% of freshwater use</a>. 
-            At the same time, climate change is <a className="external-link -freshwater" href="https://www.forbes.com/sites/paullaudicina/2018/06/07/water-day-zero-coming-to-a-city-near-you/#5df9cd5c1738" target="_blank">altering the quantity and timing of water flows</a>, 
-            leading to more severe droughts and floods and complicating conservation efforts. 
-            Our pool of usable water is also shrinking due to pollution from farms and industry that 
-            is <a className="external-link -freshwater" href="https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox/FMfcgxwJXpNbgJXPBmflxKgvvzgpFRhW?projector=1&messagePartId=0.1">triggering chemical overload</a> and coastal dead zones. In just 10 years from now, WRI projects, the world’s water supply will fall 56% short of human demand.  
+            Food demand is driving the water deficit, with agriculture responsible for <a className="external-link -freshwater" href="https://blogs.worldbank.org/opendata/chart-globally-70-freshwater-used-agriculture#:~:text=In%20most%20regions%20of%20the,percent%20increase%20in%20water%20withdrawals." target="_blank">70% of freshwater use</a>. 
+            At the same time, climate change is <a className="external-link -freshwater" href="https://www.forbes.com/sites/paullaudicina/2018/06/07/water-day-zero-coming-to-a-city-near-you/#5df9cd5c1738" target="_blank">altering the quantity and timing of water flows</a>, leading 
+            to more severe droughts and floods and complicating conservation efforts. Our pool of usable water 
+            is also shrinking due to pollution from farms and industry that is <a className="external-link -freshwater" href="https://elibrary.worldbank.org/doi/abs/10.1596/978-1-4648-1459-4#:~:text=Quality%20Unknown%3A%20The%20Invisible%20Water%20Crisis%20presents%20new%20evidence%20and,potential%2C%20and%20reduces%20food%20production." target="_blank">triggering chemical overload and coastal dead zones</a>. Within 
+            just 10 years, WRI projects, the world’s water supply will fall <a className="external-link -freshwater" href="https://www.wri.org/blog/2020/04/coronavirus-water-scarcity-hand-washing" target="_blank">56% short of human demand</a>.
         </p>,
         <p>
             So, how can we course correct? Sustainable water management by government and industry, 
-            cross-border cooperation, nature-based infrastructure and mindful consumer habits are 
-            all critical for rebalancing our water budget. A concerted global effort could deliver 
-            water security for all, and sanitation for the 3 billion people who still lack it, as 
-            soon as 2030. And it won’t break the bank, costing just over 1% of global GDP, 
-            according to <a className="external-link -freshwater" href="https://www.wri.org/blog/2020/01/cost-to-solve-global-water-crisis" target="_blank">WRI</a>.
+            cross-border cooperation, nature-based infrastructure and mindful consumer habits are all critical 
+            for rebalancing our water budget. A concerted global effort could deliver water security for all, 
+            and sanitation for the 3 billion people who still lack it, as soon as 2030. And it won’t break 
+            the bank, costing just over 1% of global GDP annually, according to <a className="external-link -freshwater" href="https://www.wri.org/blog/2020/01/cost-to-solve-global-water-crisis" target="_blank">WRI</a>. But we are not 
+            investing enough to close the gap between global renewable water supply and demand. For this 
+            we would need to invest $1.04 trillion annually in sustainable water management through 
+            2030. (source: <a className="external-link -freshwater" href="https://files.wri.org/s3fs-public/achieving-abundance.pdf" target="_blank">Achieving Abundance</a>, WRI)
         </p>,
         <p>
-            Scroll down for more on ways we can reset and rebalance our relationship with water.
         </p>
     ],
     photoAttribution: 'Photo by Dan Gold on Unsplash'
