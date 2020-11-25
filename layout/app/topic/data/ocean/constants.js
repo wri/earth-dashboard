89aebed55e709435daacd98248f87bb9675d82a1
Pablo Pareja Tobes <pablo.pareja@vizzuality.com>
2020-11-25 14:25:01 +0100

    finish ocean narrative updates

--- layout/app/topic/data/ocean/constants.js
+++ layout/app/topic/data/ocean/constants.js
@@ -6,30 +6,22 @@ export const OCEAN_HEADLINE_SECTION_DATA = {
 export const OCEAN_CHALLENGE_SECTION_DATA = {
     paragraphs: [
         <p>
-            The ocean covers 70% of our planet. Yet ocean health is declining as humanity <a className="external-link -ocean" href="https://www.sciencedirect.com/science/article/pii/S2590332219302751" target="_blank">exploits and pollutes marine life</a>. 
-            Tipping points are approaching for coral reef survival and ocean acidification as climate 
-            change warms waters. Nearly <a className="external-link -ocean" href="http://www.secore.org/site/corals/detail/coral-reefs-are-dying.23.html" target="_blank">50 percent of the world’s coral</a> 
-            has died in the last 30 years; Farm waste is fuelling hundreds of low-oxygen dead zones, 
-            some stretching for thousands of kilometers.  Since the 1970s, the area these 
-            cover has nearly doubled every year.  A third of UN-monitored fish stocks are 
-            <a className="external-link -ocean" href="http://www.fao.org/state-of-fisheries-aquaculture/en/" target="_blank">harvested beyond sustainable levels</a> 
-            while unregulated fishing <a className="external-link -ocean" href="https://news.mongabay.com/2019/10/commitments-worth-63-billion-pledged-for-ocean-protection/" target="_blank">traps up to 26 million tons of fish a year</a>. 
-            And a garbage truck's worth of plastic <a className="external-link -ocean" href="https://www.un.org/sustainabledevelopment/wp-content/uploads/2017/05/Ocean-fact-sheet-package.pdf" target="_blank">enters the ocean every minute</a>, 
-            harming marine life with unknown consequences.
+            The ocean covers 70% of our planet. Yet ocean health is declining as humanity <a className="external-link -ocean" href="https://www.sciencedirect.com/science/article/pii/S2590332219302751" target="_blank">exploits and pollutes marine life</a>. Nearly 
+            half <a className="external-link -ocean" href="http://www.secore.org/site/corals/detail/coral-reefs-are-dying.23.html" target="_blank">of the world’s coral</a> has died in the last 30 years. Farm waste is fuelling hundreds of 
+             low-oxygen dead zones. Some stretch for thousands of kilometers and the area these cover has nearly <a className="external-link -ocean" href="https://www.nationalgeographic.com/environment/oceans/dead-zones/" target="_blank">doubled every decade</a> since
+              the 1960s. A third of UN-monitored fish stocks are <a className="external-link -ocean" href="http://www.fao.org/state-of-fisheries-aquaculture/en/" target="_blank">harvested beyond sustainable levels</a>, 
+             and unregulated fishing <a className="external-link -ocean" href="https://news.mongabay.com/2019/10/commitments-worth-63-billion-pledged-for-ocean-protection/" target="_blank">traps up to 26 million tons of fish</a> a year. If this plastic pollution continues at the
+              present rate, by 2025, there could be one ton of plastic in the ocean for every three tons of fish. 
+              Plastic bags, fishing nets, and other debris choke, strangle and poison, seabirds, sea turtles and fish. 
+              Micro-plastic particles sea creatures ingest also pose a threat to humans.
         </p>,
         <p>
-            The ocean offers solutions to pressing global challenges including surging food and energy 
-            demand and climate change mitigation. Research suggests the emerging science of mariculture 
-            (ocean aquaculture) could help <a className="external-link -ocean" href="https://oceanpanel.org/sites/default/files/2020-08/Food%20from%20the%20Sea%20Final.pdf" target="_blank">deliver over 6 times more food</a> 
-            than the ocean provides us today. On the climate front, investment in sustainable, 
-            ocean-based foods and ecosystems, wind and wave energy, ocean transport and seabed carbon storage, 
-            <a className="external-link -ocean" href="https://oceanpanel.org/sites/default/files/2019-10/HLP_Report_Ocean_Solution_Climate_Change_final.pdf" target="_blank">could deliver 21% of emission</a> 
-            cuts needed to limit global warming to 1.5°C.  To bring about this future, we need a healthy and bountiful ocean.
-        </p>,
-        <p>
-            Governments, companies and citizens must act with urgency to slow these negative human pressures, 
-            in order to sustain the ocean and its potential to deliver new industries, jobs and food and 
-            energy security. This endeavor will be well worth the cost: <a className="external-link -ocean" href="https://oceanpanel.org/sites/default/files/2020-07/Ocean%20Panel_Economic%20Analysis_FINAL.pdf" target="_blank">every $ spent on a sustainable ocean economy could yield five times the return in benefits</a>. 
+            The ocean offers solutions to pressing global challenges.  It can help mitigate demand and meet surging 
+            demand for food and energy. Research suggests the emerging science of mariculture (ocean aquaculture)
+             could help <a className="external-link -ocean" href="https://oceanpanel.org/sites/default/files/2020-08/Food%20from%20the%20Sea%20Final.pdf" target="_blank">deliver over 6 times more food</a> than the ocean provides us today. On the climate front, 
+             investment in sustainable, ocean-based foods and ecosystems, wind and wave energy, ocean transport
+              and seabed carbon storage, <a className="external-link -ocean" href="https://oceanpanel.org/sites/default/files/2019-10/HLP_Report_Ocean_Solution_Climate_Change_final.pdf" target="_blank">could deliver 21% of emission cuts</a> needed to limit global warming to 1.5°C.
+               To bring about this future, we need a healthy and bountiful ocean.
         </p>
     ],
     photoAttribution: 'Photo by Francesco Ungaro on Unsplash'
@@ -39,4 +31,11 @@ export const OCEAN_DIVE_INTO_THE_DATA_DATA = {
     description: `Explore the ocean’s state of health around the world. The score evaluates yearly since 2012 the current state of food provision, artisanal fishing opportunities, natural products, carbon storage, coastal protection,tourism & recreation, coastal livelihoods and economies, sense of place, clean waters,biodiversity.`
 }
 
-export const OCEAN_METHODOLOGY_DATA = <p></p>;
+export const OCEAN_METHODOLOGY_DATA = <p>
+    The visualizations and statistics regarding global trends in the accelerating uses of ocean resources
+     were sourced from Figure 2: The Blue Acceleration of <a className="external-link -ocean" href="https://www.sciencedirect.com/science/article/pii/S2590332219302751" target="_blank">Jouffrey et al</a>. The map illustrating
+      climate model projections of the year in which coral bleaching will occur draws on <a className="external-link -ocean" href="https://coralreefwatch.noaa.gov/climate/projections/downscaled_bleaching_4km/index.php" target="_blank">NOAA</a> datasets. 
+      The charts illustrating trends in marine stocks and 2018 aquaculture and fishery
+     production are sourced from FAO’s <a className="external-link -ocean" href="http://www.fao.org/state-of-fisheries-aquaculture" target="_blank">The State of World Fisheries and Aquaculture 2020</a> report 
+     using data prepared by the <a className="external-link -ocean" href="http://www.fao.org/fishery/statistics/en" target="_blank">FAO Fisheries Division</a>.
+</p>;
