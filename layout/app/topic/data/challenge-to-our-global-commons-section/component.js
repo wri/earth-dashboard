31675c048b24fc4475f6375857a461ba4fde799b
Pablo Pareja Tobes <pablo.pareja@vizzuality.com>
2020-11-25 12:11:01 +0100

    improve challenge to our commons background

--- layout/app/topic/data/challenge-to-our-global-commons-section/component.js
+++ layout/app/topic/data/challenge-to-our-global-commons-section/component.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Scrollama, Step } from 'react-scrollama';
 import classnames from 'classnames';
 
 // utils
@@ -16,90 +15,54 @@ import styles from './challenge-to-our-global-commons-section.module.scss';
 function ChallengeToOurGlobalCommons(props) {
   const { topic } = props;
   const topicColor = getColorByTopic(topic);
-  const isBrowser = typeof window !== 'undefined';
-  const data = getChallengeSectionDataByTopic(topic);
-
-  const getTitle = () =>
-    <h1>The <span style={{ color: topicColor }}>Challenge</span> to our Global Commons</h1>;
+  const data = getChallengeSectionDataByTopic(topic);    
 
   return (
     <div
       className={styles['c-challenge-to-our-global-commons-section']}
       id="challenge-to-our-global-commons"
+      style={{ backgroundImage: `url('/static/images/${topic}/challenge.png')` }}
     >
-      <div
-        className={styles['background-container']}
-      >
-        <MediaContextProvider>
-          <Desktop>
-            <div
-              className={classnames({
-                [styles['title-container']]: true,
-                [styles['-desktop']]: true
-              })}
-              style={{ backgroundImage: `url('/static/images/${topic}/challenge.png')` }}
-            >
-              {getTitle()}
-            </div>
-          </Desktop>
-          <Mobile>
-            <div
-              className={classnames({
-                [styles['title-container']]: true,
-                [styles['-mobile']]: true
-              })}
-              style={{ backgroundImage: `url('/static/images/${topic}/challenge.png')` }}
-            >
-              {getTitle()}
-            </div>
-          </Mobile>
-
-          <div className={styles['paragraphs-container']}>
-            <div className={styles.paragraphs}>
-              {isBrowser &&
-                <Scrollama
-                  offset={0.6}
-                  onStepExit={({ data }) => console.log('onStepExit', data)}
-                >
-                  {data?.paragraphs?.map((paragraph, index) =>
-                    <Step
-                      data={index}
-                      key={`challenge-step-${index}`}
-                    >
-                      <div>
-                        <Desktop>
-                          <div
-                            className={classnames({
-                              [styles['challenge-paragraph']]: true,
-                              [styles[`-${topic}`]]: true,
-                              [styles['-desktop']]: true
-                            })}
-                          >
-                            {paragraph}
-                          </div>
-                        </Desktop>
-                        <Mobile>
-                          <div
-                            className={classnames({
-                              [styles['challenge-paragraph']]: true,
-                              [styles[`-${topic}`]]: true,
-                              [styles['-mobile']]: true
-                            })}
-                          >
-                            {paragraph}
-                          </div>
-                        </Mobile>
-                      </div>
-                    </Step>
-                  )}
-                </Scrollama>
-              }
-            </div>
+      <MediaContextProvider>
+        <Desktop>
+          <h1>The <span style={{ color: topicColor }}>Challenge</span> to our Global Commons</h1>
+        </Desktop>
+        <Mobile>
+          <h1>The <span style={{ color: topicColor }}>Challenge</span> to our Global Commons</h1>
+        </Mobile>
+        <div className={styles['paragraphs-container']}>
+          <div className={styles.paragraphs}>
+            {data?.paragraphs?.map((paragraph, index) =>
+              <div key={`paragraph-${index}`}>
+                <Desktop>
+                  <div
+                    className={classnames({
+                      [styles['challenge-paragraph']]: true,
+                      [styles[`-${topic}`]]: true,
+                      [styles['-desktop']]: true
+                    })}
+                  >
+                    {paragraph}
+                  </div>
+                </Desktop>
+                <Mobile>
+                  <div
+                    className={classnames({
+                      [styles['challenge-paragraph']]: true,
+                      [styles[`-${topic}`]]: true,
+                      [styles['-mobile']]: true
+                    })}
+                  >
+                    {paragraph}
+                  </div>
+                </Mobile>
+              </div>
+            )}
           </div>
-        </MediaContextProvider>
-        <div className={styles['photo-attribution-container']}>
-          {data?.photoAttribution}
         </div>
+      </MediaContextProvider>
+      <div className={styles['photo-attribution-container']}>
+        {data?.photoAttribution}
       </div>
     </div>
   );
