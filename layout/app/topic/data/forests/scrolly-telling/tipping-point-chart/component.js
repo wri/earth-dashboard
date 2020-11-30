e06b33aac5b9777ff9621718d447a59eede45078
Pablo Pareja Tobes <pablo.pareja@vizzuality.com>
2020-11-30 15:57:12 +0100

    tipping point forest chart fixed for different screen sizes

--- layout/app/topic/data/forests/scrolly-telling/tipping-point-chart/component.js
+++ layout/app/topic/data/forests/scrolly-telling/tipping-point-chart/component.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import classnames from 'classnames';
 
 // components
 import PulsatingItem from 'components/ui/pulsating-item';
@@ -19,20 +20,35 @@ import styles from './tipping-point-chart.module.scss';
 import { TIPPING_POINT_CHART_SVG } from './data';
 
 function TippingPointChart() {
-    const getContent = (mode) => <>
-        <div className={styles.chart}>
-            {TIPPING_POINT_CHART_SVG}
-        </div>
-        <div className={styles.marker}>
-            <PulsatingItem />
-        </div>
-    </>;
+    const getContent = (mode) =>
+        <div className={styles['chart-container']}>
+            <div className={classnames({
+                [styles.chart]: true,
+                [styles['-desktop']]: mode === 'desktop',
+                [styles['-mobile']]: mode === 'mobile',
+                [styles['-desktop-large']]: mode === 'desktop-large',
+                [styles['-desktop-x-large']]: mode === 'desktop-x-large'
+            })}
+            >
+                {TIPPING_POINT_CHART_SVG}
+            </div>
+            <div className={classnames({
+                [styles.marker]: true,
+                [styles['-desktop']]: mode === 'desktop',
+                [styles['-mobile']]: mode === 'mobile',
+                [styles['-desktop-large']]: mode === 'desktop-large',
+                [styles['-desktop-x-large']]: mode === 'desktop-x-large'
+            })}
+            >
+                <PulsatingItem level={2} active={true} />
+            </div>
+        </div>;
     return (
         <div className={styles['c-tipping-point-chart']}>
             <MediaContextProvider>
                 <Mobile>{getContent('mobile')}</Mobile>
-                <Desktop>{getContent('desktop')}</Desktop>
-                <DesktopLarge>{getContent('desktop-large')}</DesktopLarge>
+                <Desktop includeBiggerScreens={false}>{getContent('desktop')}</Desktop>
+                <DesktopLarge includeBiggerScreens={false}>{getContent('desktop-large')}</DesktopLarge>
                 <DesktopXLarge>{getContent('desktop-x-large')}</DesktopXLarge>
             </MediaContextProvider>
         </div>
