9be7b1314fb4dabb9c884f19e2aaa5bcc9d02e6d
Pablo Pareja Tobes <pablo.pareja@vizzuality.com>
2020-12-01 11:07:33 +0100

    first changes for homepage mobile version

--- layout/header/component.js
+++ layout/header/component.js
@@ -95,7 +95,7 @@ function Header(props) {
       case SHARE_HEADER_TAB:
         return (
           <div className={styles['share-container']}>
-            <ShareBox 
+            <ShareBox
               url={isServer ? '' : window.location.href}
               style={{ borderColor: '#1A2129' }}
             />
@@ -115,6 +115,38 @@ function Header(props) {
     <div className={styles['logo-container']}>
       {getLogo()}
     </div>;
+
+  const getNavigationTags = (mobile) =>
+    <ul className={classnames({
+        [styles['navigation-tabs']]: true,
+        [styles['-desktop']]: !mobile,
+        [styles['-mobile']]: mobile
+      })}
+    >
+      <li
+        className={classnames({
+          [styles['-active']]: tab === SITE_NAVIGATION_HEADER_TAB
+        })}
+        onClick={() => setTab(SITE_NAVIGATION_HEADER_TAB)}
+      >
+        <a>SITE NAVIGATION</a>
+      </li>
+      <li
+        className={classnames({
+          [styles['-active']]: tab === ABOUT_HEADER_TAB
+        })}
+        onClick={() => setTab(ABOUT_HEADER_TAB)}
+      >
+        <a>ABOUT</a>
+      </li>
+      <li className={classnames({
+        [styles['-active']]: tab === SHARE_HEADER_TAB
+      })}
+        onClick={() => setTab(SHARE_HEADER_TAB)}
+      >
+        <a>SHARE</a>
+      </li>
+    </ul>;
 
   return (
     <header className={styles.header}>
@@ -147,37 +179,7 @@ function Header(props) {
                       [styles['-desktop']]: true
                     })}>
                       {getLogo()}
-                      <ul className={styles['left-links']}>
-                        <li
-                          className={classnames({
-                            [styles['-active']]: tab === SITE_NAVIGATION_HEADER_TAB
-                          })}
-                          onClick={() => setTab(SITE_NAVIGATION_HEADER_TAB)}
-                        >
-                          <a>SITE NAVIGATION</a>
-                        </li>
-                        <li
-                          className={classnames({
-                            [styles['-active']]: tab === ABOUT_HEADER_TAB
-                          })}
-                          onClick={() => setTab(ABOUT_HEADER_TAB)}
-                        >
-                          <a>ABOUT</a>
-                        </li>
-                        <li className={classnames({
-                          [styles['-active']]: tab === SHARE_HEADER_TAB
-                        })}
-                          onClick={() => setTab(SHARE_HEADER_TAB)}
-                        >
-                          <a>SHARE</a>
-                        </li>
-                      </ul>
-                      {/* <div className={classnames({
-                        [styles['powered-by']]: true,
-                        [styles['-desktop']]: true
-                      })}>
-                        powered by <a href="https://resourcewatch.org/" target="_blank">RESOURCEWATCH</a>
-                      </div> */}
+                      {getNavigationTags(false)}
                     </div>
                     <div className={classnames({
                       [styles['right-container']]: true,
@@ -219,23 +221,7 @@ function Header(props) {
                       [styles['left-container']]: true,
                       [styles['-mobile']]: true
                     })}>
-                      {getLogo()}
-                      <ul className={styles['left-links']}>
-                        <li>About</li>
-                        <li>
-                          <div className={styles['share-icons-container']}>
-                            <img src="/static/images/share/email_white.svg" />
-                            <img src="/static/images/share/facebook_white.svg" />
-                            <img src="/static/images/share/twitter_white.svg" />
-                          </div>
-                        </li>
-                      </ul>
-                      <div className={classnames({
-                        [styles['powered-by']]: true,
-                        [styles['-mobile']]: true
-                      })}>
-                        powered by <a href="https://resourcewatch.org/" target="_blank">RESOURCEWATCH</a>
-                      </div>
+                      {getNavigationTags(true)}
                     </div>
                     <div className={classnames({
                       [styles['right-container']]: true,
@@ -255,12 +241,12 @@ function Header(props) {
   );
 }
 
-Header.propTypes = { 
+Header.propTypes = {
   showLogo: PropTypes.bool.isRequired,
   openMenu: PropTypes.bool,
   selectedTab: PropTypes.string
 };
-Header.defaultProps = { 
+Header.defaultProps = {
   showLogo: true,
   selectedTab: SITE_NAVIGATION_HEADER_TAB,
   openMenu: false
