ba8f1e7dead813ca4e7ad90bf00176cb31a4c7cb
Pablo Pareja Tobes <pablo.pareja@vizzuality.com>
2020-12-01 11:43:46 +0100

    add missing underline in about page

--- layout/header/about/component.js
+++ layout/header/about/component.js
@@ -11,13 +11,13 @@ function About() {
                 We created the Earth Dashboard as a “Situation Room” for the planet.  It is an interactive platform that tracks the forces imperiling the natural systems that support life on earth. It presents a constant flow of reliable, accessible scientific data on what can and must be done to protect the global commons – vital resources and ecosystems – and confront an accelerating planetary emergency.
             </p>
             <p>
-                The Earth Dashboard is a joint project of the <a className="external-link" target="_blank" href="https://globalcommonsalliance.org/" >Global Commons Alliance</a>, the
+                The Earth Dashboard is a joint project of the <a className="external-link -white" target="_blank" href="https://globalcommonsalliance.org/" >Global Commons Alliance</a>, the
                 largest coalition of scientific and conservation organizations,
-                 and <a className="external-link" target="_blank" href="https://resourcewatch.org/" >Resource Watch</a>, an open
+                 and <a className="external-link -white" target="_blank" href="https://resourcewatch.org/" >Resource Watch</a>, an open
                  source platform which compiles hundreds of visualized data sets from the world’s most
                  reliable sources, in partnership with popular environmental news
-                 site <a className="external-link" target="_blank" href="https://news.mongabay.com/" >Mongabay</a>, and leading
-                 digital news distributer <a className="external-link" target="_blank" href="https://nowthisnews.com/earth" >NowThis Earth</a>.
+                 site <a className="external-link -white" target="_blank" href="https://news.mongabay.com/" >Mongabay</a>, and leading
+                 digital news distributer <a className="external-link -white" target="_blank" href="https://nowthisnews.com/earth" >NowThis Earth</a>.
                  It offers activists, the media, policymakers, business leaders and the public a unique tool – a way to find
                  vital information, monitor the latest developments, set priorities, and take action.  It is designed
                  to be comprehensive, detailed, transparent, open source, and easy to share. The facts and findings on
