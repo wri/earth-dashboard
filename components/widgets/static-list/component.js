cedffa07ebb944c9268403567eecb10b29531d71
Pablo Pareja Tobes <pablo.pareja@vizzuality.com>
2020-12-02 13:11:12 +0100

    list widget styles adjustments

--- components/widgets/static-list/component.js
+++ components/widgets/static-list/component.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classnames from 'classnames';
 
 // components
 import SourceBox from 'components/widgets/source-box';
@@ -11,7 +12,7 @@ function StaticListWidget(props) {
   const { widget, showSource } = props;
   const widgetConfig = widget && widget.widgetConfig;
   const staticListWidgetConfig = widgetConfig && widgetConfig.staticListWidgetConfig;
-  const { numbers, bullets, values, source } = staticListWidgetConfig || {};
+  const { numbers, bullets, values, source, topic } = staticListWidgetConfig || {};
   const ListTag = numbers ? 'ol' : (bullets ? 'ul' : 'ol');
 
   return (
@@ -19,29 +20,39 @@ function StaticListWidget(props) {
       <ListTag>
         {values.map(elem =>
           <li key={`list-elem-${elem.label}`}>
-            {elem.link && 
-              <a 
-                href={elem.link} 
-                target="_blank" 
+            {elem.link &&
+              <a
+                href={elem.link}
+                target="_blank"
                 style={elem.style}
               >
                 {elem.label}
               </a>}
-            {!elem.link && <span style={elem.style}>{elem.label}</span>}
+            {!elem.link &&
+              <span
+                className={classnames({
+                  [styles['list-element-label']]: true,
+                  [topic]: true
+                })}
+                style={elem.style}
+              >
+                {elem.label}
+              </span>
+            }
           </li>
         )}
       </ListTag>
-      { showSource && source && <SourceBox source={source} /> }
+      { showSource && source && <SourceBox source={source} />}
     </div>
   );
 }
 
-StaticListWidget.propTypes = { 
+StaticListWidget.propTypes = {
   widget: PropTypes.object.isRequired,
   showSource: PropTypes.bool
 };
 
-StaticListWidget.defaultProps = { 
+StaticListWidget.defaultProps = {
   showSource: true
 };
 
