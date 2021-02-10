cb3ef08a0a79b90b1b86ac4b6b77013b5dc76a04
Tiago Garcia <tiagojsag@gmail.com>
2021-02-10 17:43:01 +0100

    Add staging deployment config

---
+++ config/deploy/staging.rb
@@ -0,0 +1,13 @@
+server '54.197.134.233', user: 'ubuntu', roles: %w{web app db}, primary: true
+
+set :ssh_options, forward_agent: true
+
+set :branch, 'develop'
+
+set :default_env, {
+  'ED_NODE_ENV' => 'production',
+  'WRI_API_URL' => 'https://api.resourcewatch.org/v1',
+  'CONTROL_TOWER_URL' => 'https://api.resourcewatch.org',
+  'CALLBACK_URL' => 'https://earthhq.org/sign-in',
+  'APPLICATIONS' => 'earthhq'
+}
