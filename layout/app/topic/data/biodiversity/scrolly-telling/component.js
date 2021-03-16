df169d8bde8abdd7ae22eba8128c3bc10de0ef3f
Pablo Pareja Tobes <pablo.pareja@vizzuality.com>
2021-03-16 12:17:20 +0100

    progress on biodiversity globe tests

--- layout/app/topic/data/biodiversity/scrolly-telling/component.js
+++ layout/app/topic/data/biodiversity/scrolly-telling/component.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Scrollama, Step } from 'react-scrollama';
 import classnames from 'classnames';
@@ -23,8 +23,26 @@ function BiodiversityScrollyTelling({ topic }) {
   const [currentStepIndex, setCurrentStepIndex] = useState(0);
   const isBrowser = typeof window !== 'undefined';
   const currentStep = BIODIVERSITY_STEPS[currentStepIndex];
+  const { location, autoRotate } = currentStep;
+  const globe = useRef();
 
-  console.info('topic', topic);
+  useEffect(() => {
+    if (globe) {
+      globe.current.controls().enableZoom = false;
+    }
+  }, [globe]);
+
+  useEffect(() => {
+    if (globe) {
+      globe.current.pointOfView(location, 2000);
+
+      if (autoRotate) {
+        globe.current.controls().autoRotate = true;
+      } else {
+        globe.current.controls().autoRotate = false;
+      }
+    }
+  }, [location, autoRotate]);
 
   // This callback fires when a Step hits the offset threshold. It receives the
   // data prop of the step, which in this demo stores the index of the step.
@@ -58,6 +76,13 @@ function BiodiversityScrollyTelling({ topic }) {
         <div className={styles.story}>
           <div className={styles['sticky-container']}>
             <div className={styles['wrapper-container']}>
+              {isBrowser && (
+                <Globe
+                  ref={globe}
+                  globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+                  className="globe"
+                />
+              )}
               {currentStep.stickyContainerElement && (
                 <>
                   <Desktop className={classnames({
@@ -65,11 +90,6 @@ function BiodiversityScrollyTelling({ topic }) {
                     [styles['-desktop']]: true
                   })}
                   >
-                    {isBrowser && (
-                      <Globe
-                        globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
-                      />
-                    )}
                     {currentStep.stickyContainerElement}
                   </Desktop>
                   <Mobile className={classnames({
--- layout/app/topic/data/biodiversity/scrolly-telling/constants.js
+++ layout/app/topic/data/biodiversity/scrolly-telling/constants.js
@@ -9,11 +9,12 @@ export const BIODIVERSITY_STEPS = [
         <>
           <h6 className="biodiversity">Biodiversity step 1</h6>
           <p>
-            Lalalalla
+            Hey!
           </p>
         </>
       )
     },
+    location: { lat: 13, lng: -12, altitude: 2 },
     stickyContainer: <div />,
     visualSource: <a href="https://www.mcc-berlin.net/en/research/co2-budget.html" target="_blank" rel="noreferrer">MCC</a>,
     visualDataset: <a href="https://www.ipcc.ch/sr15/" target="_blank" rel="noreferrer">IPCC</a>
@@ -25,11 +26,47 @@ export const BIODIVERSITY_STEPS = [
         <>
           <h6 className="biodiversity">Biodiversity step 2</h6>
           <p>
-            Lalalalla
+            Let's move around a bit
           </p>
         </>
       )
     },
+    location: { lat: 23, lng: 42, altitude: 2 },
+    stickyContainer: <div />,
+    visualSource: <a href="https://www.mcc-berlin.net/en/research/co2-budget.html" target="_blank" rel="noreferrer">MCC</a>,
+    visualDataset: <a href="https://www.ipcc.ch/sr15/" target="_blank" rel="noreferrer">IPCC</a>
+  },
+  {
+    index: 2,
+    textPanel: {
+      text: (
+        <>
+          <h6 className="biodiversity">Biodiversity step 3</h6>
+          <p>
+            Now moving but also zooming in
+          </p>
+        </>
+      )
+    },
+    location: { lat: -3, lng: 2, altitude: 0.3 },
+    stickyContainer: <div />,
+    visualSource: <a href="https://www.mcc-berlin.net/en/research/co2-budget.html" target="_blank" rel="noreferrer">MCC</a>,
+    visualDataset: <a href="https://www.ipcc.ch/sr15/" target="_blank" rel="noreferrer">IPCC</a>
+  },
+  {
+    index: 3,
+    textPanel: {
+      text: (
+        <>
+          <h6 className="biodiversity">Biodiversity step 4</h6>
+          <p>
+            Now zooming out and also set on rotation forever
+          </p>
+        </>
+      )
+    },
+    location: { lat: -3, lng: 2, altitude: 3 },
+    autoRotate: true,
     stickyContainer: <div />,
     visualSource: <a href="https://www.mcc-berlin.net/en/research/co2-budget.html" target="_blank" rel="noreferrer">MCC</a>,
     visualDataset: <a href="https://www.ipcc.ch/sr15/" target="_blank" rel="noreferrer">IPCC</a>
