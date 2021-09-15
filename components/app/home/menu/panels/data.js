1e2c0de1a88589498c910cc0bd084bd57009ffb2
Kevin Borrill <kevin@3sidedcube.com>
2021-09-15 14:37:16 +0100

    Move data selection to own component

--- components/app/home/menu/panels/data.js
+++ components/app/home/menu/panels/data.js
@@ -1,8 +1,8 @@
 import { useMemo } from "react";
 import PropTypes from "prop-types";
-import ToggleList from "components/ui/toggle-list";
+import DataLayers from "components/ui/toggle-list/data-list";
 import ToggleItem from "components/ui/toggle-list/toggle-item";
-import InfoLabel from "components/ui/toggle-list/info-label";
+import ToggleList from "components/ui/toggle-list";
 import classnames from "classnames";
 import styles from "../menu.module.scss";
 import useDataLayers from "hooks/useDataLayers";
@@ -75,93 +75,30 @@ const DataPanel = ({
         </ToggleList>
         <p className={styles["c-home-menu__template-description"]}>{currentTemplate.attributes.description}</p>
         <div className={styles["c-home-menu__data-selection"]}>
-          {datasetLayers.length > 0 && (
-            <>
-              {!isMobile && <InfoLabel title="Dataset" onSelectInfo={onSelectInfo} />}
-              <ToggleList
-                selectedValue={datasetValue}
-                onSelect={value => setDatasetValue(value)}
-                title="Datasets"
-                hasLegend={isMobile}
-                legendComponent={
-                  <InfoLabel title="Dataset" className="u-margin-right-xs" onSelectInfo={onSelectInfo} />
-                }
-                role="group"
-                aria-labelledby="dataset-label"
-                className={styles["c-home-menu__toggle-list"]}
-                singularMode
-              >
-                {datasetLayers.map(layer => (
-                  <ToggleItem
-                    key={layer.attributes.data_key}
-                    value={layer.attributes.data_key}
-                    className="u-margin-right-xxs u-margin-bottom-xs"
-                    type="checkbox"
-                  >
-                    {layer.attributes.title}
-                  </ToggleItem>
-                ))}
-              </ToggleList>
-            </>
-          )}
-          {monitorLayers.length > 0 && (
-            <>
-              {!isMobile && <InfoLabel title="Monitor" onSelectInfo={onSelectInfo} />}
-              <ToggleList
-                selectedValue={monitorValue}
-                onSelect={value => setMonitorValue(value)}
-                title="Monitor"
-                hasLegend={isMobile}
-                legendComponent={
-                  <InfoLabel title="Monitor" className="u-margin-right-xs" onSelectInfo={onSelectInfo} />
-                }
-                role="group"
-                aria-labelledby="monitor-label"
-                className={styles["c-home-menu__toggle-list"]}
-                singularMode
-              >
-                {monitorLayers.map(layer => (
-                  <ToggleItem
-                    key={layer.attributes.data_key}
-                    value={layer.attributes.data_key}
-                    className="u-margin-right-xxs u-margin-bottom-xs"
-                    type="checkbox"
-                  >
-                    {layer.attributes.title}
-                  </ToggleItem>
-                ))}
-              </ToggleList>
-            </>
-          )}
-          {animationLayers.length > 0 && (
-            <>
-              {!isMobile && <InfoLabel title="Animation" onSelectInfo={onSelectInfo} />}
-              <ToggleList
-                selectedValue={animationValue}
-                onSelect={value => setAnimationValue(value)}
-                title="Animation"
-                hasLegend={isMobile}
-                legendComponent={
-                  <InfoLabel title="Animation" className="u-margin-right-xs" onSelectInfo={onSelectInfo} />
-                }
-                role="group"
-                aria-labelledby="animation-label"
-                className={styles["c-home-menu__toggle-list"]}
-                singularMode
-              >
-                {animationLayers.map(layer => (
-                  <ToggleItem
-                    key={layer.attributes.data_key}
-                    value={layer.attributes.data_key}
-                    className="u-margin-right-xxs u-margin-bottom-xs"
-                    type="checkbox"
-                  >
-                    {layer.attributes.title}
-                  </ToggleItem>
-                ))}
-              </ToggleList>
-            </>
-          )}
+          <DataLayers
+            title="Dataset"
+            onSelectInfo={onSelectInfo}
+            isMobile={isMobile}
+            value={datasetValue}
+            setValue={setDatasetValue}
+            layers={datasetLayers}
+          />
+          <DataLayers
+            title="Monitor"
+            onSelectInfo={onSelectInfo}
+            isMobile={isMobile}
+            value={monitorValue}
+            setValue={setMonitorValue}
+            layers={monitorLayers}
+          />
+          <DataLayers
+            title="Monitor"
+            onSelectInfo={onSelectInfo}
+            isMobile={isMobile}
+            value={animationValue}
+            setValue={setAnimationValue}
+            layers={animationLayers}
+          />
         </div>
         {description && <p className="u-margin-none">{description}</p>}
         {source && <p className="u-margin-none">{source}</p>}
