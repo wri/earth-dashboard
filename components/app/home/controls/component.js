68576c97d0675d10781372b3065c04edfb435a16
Owen Evans <owen@3sidedcube.com>
2021-09-21 15:10:45 +0100

    lint fix

--- components/app/home/controls/component.js
+++ components/app/home/controls/component.js
@@ -12,7 +12,9 @@ const ControlsConfig = [
 const Controls = () => (
   <div className={classnames(styles["c-controls-container"])}>
     {ControlsConfig.map(control => (
-      <button key={control.key} className={classnames(styles["c-control-btn"])}><img src={`/static/icons/${control.src}`} width="48px" height="48px" /></button>
+      <button key={control.key} className={classnames(styles["c-control-btn"])}>
+        <img src={`/static/icons/${control.src}`} width="48px" height="48px" />
+      </button>
     ))}
   </div>
 );
