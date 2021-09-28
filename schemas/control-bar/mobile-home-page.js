9ee3bdc81d8fafe245dd8f382ea1665be66fbd43
Owen Evans <owen@3sidedcube.com>
2021-09-28 15:10:38 +0100

    available on mobile and design self review

--- schemas/control-bar/mobile-home-page.js
+++ schemas/control-bar/mobile-home-page.js
@@ -1,6 +1,7 @@
+import projectionTypeToggle from "constants/control-bar/controls/projectionTypeToggle";
 import settings from "constants/control-bar/controls/settings";
 import location from "constants/control-bar/controls/location";
 
-const MobileHomePageItems = [location, settings];
+const MobileHomePageItems = [projectionTypeToggle, location, settings];
 
 export default MobileHomePageItems;
