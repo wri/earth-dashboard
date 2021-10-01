add0bb39cf56519dc5cd9aa248ca85f6bec70404
Owen Evans <owen@3sidedcube.com>
2021-10-01 15:49:42 +0100

    video now plays inline when thumbnail is pressed

--- components/video-article/component.js
+++ components/video-article/component.js
@@ -1,28 +1,55 @@
+import { useState } from "react";
 import Image from "next/image";
+import ReactPlayer from 'react-player';
 import classnames from "classnames";
+import { CSSTransition } from "react-transition-group";
 import styles from "./video-article.module.scss";
 import YouTubePlayIcon from "public/static/icons/youtube-play.svg";
 import PropTypes from "prop-types";
 
-const VideoArticle = ({ className, title, duration, image, videoURL }) => (
-  <article className={classnames(className, styles["c-video-article"])}>
-    <Image src={image} layout="fill" objectFit="cover" role="presentation" alt="" />
+const VideoArticle = ({ className, title, duration, image, videoURL }) => {
+  const [isPlaying, setIsPlaying] = useState(false);
 
-    <div className={styles["c-video-article__duration-wrap"]}>
-      <span className={styles["c-video-article__duration"]}>{duration}</span>
-    </div>
+  const videoPlayerStyles = {
+    position: "absolute",
+    top: "0",
+    right: "0",
+    bottom: "0",
+    left: "0"
+  };
 
-    <div className={styles["c-video-article__play-icon-wrap"]}>
-      <button>
-        <Image src={YouTubePlayIcon} role="presentation" alt="" />
-      </button>
-    </div>
+  return (
+    <article className={classnames(className, styles["c-video-article"])} onClick={() => setIsPlaying(true)}>
+      <ReactPlayer style={videoPlayerStyles} width="100%" height="100%" playing={isPlaying} controls={true} url={videoURL} />
 
-    <div className={styles["c-video-article__title-wrap"]}>
-      <span className={styles["c-video-article__title"]}>{title}</span>
-    </div>
-  </article>
-);
+      <CSSTransition in={!isPlaying} appear={!isPlaying} timeout={parseInt(styles.transition)} classNames={{
+        exit: styles["c-video-article-thumbnail"],
+        exitActive: styles["c-video-article-thumbnail--hiding"],
+        exitDone: styles["c-video-article-thumbnail--hidden"],
+      }}>
+        <div>
+          <Image src={image} layout="fill" objectFit="cover" role="presentation" alt="" />
+
+          <div className={styles["c-video-article__content"]}>
+            <div className={styles["c-video-article__duration-wrap"]}>
+              <span className={styles["c-video-article__duration"]}>{duration}</span>
+            </div>
+
+            <div className={styles["c-video-article__play-icon-wrap"]}>
+              <button>
+                <Image src={YouTubePlayIcon} role="presentation" alt="" />
+              </button>
+            </div>
+
+            <div className={styles["c-video-article__title-wrap"]}>
+              <span className={styles["c-video-article__title"]}>{title}</span>
+            </div>
+          </div>
+        </div>
+      </CSSTransition>
+    </article>
+  )
+};
 
 VideoArticle.propTypes = {
   className: PropTypes.string,
