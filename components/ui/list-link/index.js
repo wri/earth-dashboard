69ba23a2a0e1d2c2fca0e13a27912a35960e7f23
Owen Evans <owen@3sidedcube.com>
2021-10-08 08:46:40 +0100

    lint fix

--- components/ui/list-link/index.js
+++ components/ui/list-link/index.js
@@ -4,18 +4,16 @@ import PropTypes from "prop-types";
 
 const ListLink = ({ className, link, label, external, ...rest }) => {
   const anchorProps = Object.assign(
-    {...rest},
+    { ...rest },
     external && { target: "_blank", rel: "nofollow noreferrer" },
     // Remove some attributes that shouldn't be applied to the anchor element
     { image: null, body: null }
   );
-  
+
   return (
     <li className={classnames(className)}>
       <Link href={link}>
-        <a {...anchorProps}>
-          {label}
-        </a>
+        <a {...anchorProps}>{label}</a>
       </Link>
     </li>
   );
