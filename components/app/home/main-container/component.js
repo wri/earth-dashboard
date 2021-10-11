03b6c8c8a03a8b2a555ec4330be89da24477bc1d
Kevin Borrill <kevin@3sidedcube.com>
2021-10-11 13:34:39 +0100

    Add menu to Dialog menu

--- components/app/home/main-container/component.js
+++ components/app/home/main-container/component.js
@@ -21,7 +21,6 @@ import DatePickerMenu from "../date-picker-menu";
 const MainContainer = ({ isMobile, setIsMobile, setModes, layersLabelArr, dateOfDataShown, shouldFadeControls }) => {
   const [hasMenuOpen, setHasMenuOpen] = useState(false);
   const [hasIframe, setHasIframe] = useState(false);
-  const [isClosingMenu, setIsClosingMenu] = useState(false);
   const [isFetchingTemplates, setIsFetchingTemplates] = useState(null);
   const [homePageMapControlsItems, setHomePageControlBarItems] = useState([]);
 
@@ -50,15 +49,7 @@ const MainContainer = ({ isMobile, setIsMobile, setModes, layersLabelArr, dateOf
   }, [overlayLayer?.product]);
 
   const toggleMenu = () => {
-    if (!hasMenuOpen) {
-      setHasMenuOpen(true);
-    } else {
-      setIsClosingMenu(true);
-      setTimeout(() => {
-        setIsClosingMenu(false);
-        setHasMenuOpen(false);
-      }, 400);
-    }
+    setHasMenuOpen(!hasMenuOpen);
   };
 
   // Store the isMobile flag in the redux store
@@ -115,13 +106,13 @@ const MainContainer = ({ isMobile, setIsMobile, setModes, layersLabelArr, dateOf
           scaleGradient={overlayLayer.product.scale.getCss(180)}
         />
       )}
-      {hasMenuOpen && !isFetchingTemplates && (
+      {!isFetchingTemplates && (
         <Menu
           isMobile={isMobile}
+          isOpen={hasMenuOpen}
           onClose={toggleMenu}
           id="menu"
           ref={menuRef}
-          isClosing={isClosingMenu}
           earthServer={earthServer.current}
           layers={layers}
         />
--- components/app/home/menu/component.js
+++ components/app/home/menu/component.js
@@ -6,25 +6,8 @@ import PropTypes from "prop-types";
 import DataPanel from "./panels/data";
 import HeadlinePanel from "./panels/headlines";
 import DataHighlightsPanel from "./panels/dataHighlights";
-
-const INFO_DATA = {
-  dataset: {
-    title: "Dataset",
-    description: `Datasets show another dimension of data using color. Some overlays are valid at a specific height while others are valid for the entire thickness of the atmosphere
-
-    Wind	- wind speed at specified height
-    Temp	- temperature at specified height
-    RH	- relative humidity at specified height`
-  },
-  monitor: {
-    title: "Monitor",
-    description: "Lorem Ipsum"
-  },
-  animation: {
-    title: "Animation",
-    description: "Lorem Ipsum"
-  }
-};
+import useDialogPanel from "hooks/useDialogPanel";
+import DialogPanel from "components/app/home/dialog-panel";
 
 const INFO_TAB_INDEX = 3;
 const DATA_TAB_INDEX = 2;
@@ -32,9 +15,9 @@ const DATA_TAB_INDEX = 2;
 const Menu = forwardRef(
   (
     {
+      isOpen,
       isMobile,
       onClose,
-      isClosing,
       modes,
       currentMode,
       setCurrentMode,
@@ -54,6 +37,8 @@ const Menu = forwardRef(
     },
     ref
   ) => {
+    const { shouldAnimate, handleClose } = useDialogPanel(isOpen, onClose);
+
     const [tabIndex, setTabIndex] = useState(0);
     const [infoData, setInfoData] = useState(null);
     const [forceInfoPage, setForceInfoPage] = useState(false);
@@ -69,109 +54,105 @@ const Menu = forwardRef(
       setInfoData(null);
     };
 
-    const onSelectInfo = key => {
-      setTabIndex(INFO_TAB_INDEX);
-      setInfoData(INFO_DATA[key]);
-    };
-
     return (
-      <div
-        className={classnames(styles["c-home-menu-container"], isClosing && styles["c-home-menu-container--closing"])}
-      >
-        <div
-          className={classnames(
-            styles["c-home-menu"],
-            isClosing && styles["c-home-menu--closing"],
-            isInfoPage && styles["c-home-menu--is-info-page"]
-          )}
-          {...rest}
+      isOpen && (
+        <DialogPanel
+          onClose={handleClose}
+          isMobile={isMobile}
+          shouldAnimate={shouldAnimate}
+          className={styles["c-home-menu-container"]}
         >
-          <Tabs
-            selectedIndex={tabIndex}
-            onSelect={index => setTabIndex(index)}
-            className={styles["c-home-menu__tabs"]}
-            domRef={el => {
-              if (ref) {
-                ref.current = el;
-              }
-            }}
+          <div
+            className={classnames(styles["c-home-menu"], isInfoPage && styles["c-home-menu--is-info-page"])}
+            {...rest}
           >
-            <div className={classnames(styles["c-home-menu__header"])}>
-              <div className={classnames(styles["c-home-menu__header-content"])}>
-                {!isInfoPage && <h2 className={styles["c-home-menu__header-text"]}>Understand the emergency</h2>}
-                {isInfoPage && (
-                  <>
-                    <button className={styles["c-home-menu__back-button"]} onClick={onBack} aria-label="Back" />
-                    {infoData && isMobile && (
-                      <h2 className={classnames(styles["c-home-menu__header-text"], "u-text-center")}>
-                        {infoData.title}
-                      </h2>
-                    )}
-                  </>
-                )}
-                {onClose && (
-                  <button className={styles["c-home-menu__close-button"]} onClick={onClose} aria-label="Close menu" />
-                )}
-              </div>
-              <TabList className={classnames(styles["c-home-menu__tab-list"], "u-padding-top-xs")}>
-                <Tab
-                  className={classnames(styles["c-home-menu__tab"], "u-margin-right-l")}
-                  data-testid="tab-1"
-                  ref={ref}
-                >
-                  Climate Alerts
-                </Tab>
-                <Tab className={classnames(styles["c-home-menu__tab"], "u-margin-right-l")} data-testid="tab-2">
-                  Data Highlights
-                </Tab>
-                <Tab className={styles["c-home-menu__tab"]} data-testid="tab-3">
-                  Advanced Menu
-                </Tab>
-                <Tab className="u-display-none" data-testid="tab-4">
-                  Info
-                </Tab>
-              </TabList>
-            </div>
-            <div className={classnames(styles["c-home-menu__content"], "u-padding-none")}>
-              <div className={classnames(styles["c-home-menu__tab-container"])}>
-                <TabPanel className={styles["c-home-menu__tab-panel"]} data-testid="panel-1">
-                  <HeadlinePanel onForceInfoPage={() => setForceInfoPage(true)} forceInfoPage={forceInfoPage} />
-                </TabPanel>
-                <TabPanel className={styles["c-home-menu__tab-panel"]} data-testid="panel-2">
-                  <DataHighlightsPanel />
-                </TabPanel>
-                <TabPanel className={styles["c-home-menu__tab-panel"]} data-testid="panel-3">
-                  <DataPanel
-                    currentMode={currentMode}
-                    setCurrentMode={setCurrentMode}
-                    modes={modes}
-                    datasetValue={datasetValue}
-                    setDatasetValue={setDatasetValue}
-                    monitorValue={monitorValue}
-                    heightValue={heightValue}
-                    setHeightValue={setHeightValue}
-                    setMonitorValue={setMonitorValue}
-                    animationValue={animationValue}
-                    animationEnabled={animationEnabled}
-                    setAnimationValue={setAnimationValue}
-                    isMobile={isMobile}
-                    layers={layers}
-                    // TODO Refactor Data panel to pull from redux instead of passing props from parent.
-                  />
-                </TabPanel>
-                <TabPanel>
-                  {infoData && (
+            <Tabs
+              selectedIndex={tabIndex}
+              onSelect={index => setTabIndex(index)}
+              className={styles["c-home-menu__tabs"]}
+              domRef={el => {
+                if (ref) {
+                  ref.current = el;
+                }
+              }}
+            >
+              <div className={classnames(styles["c-home-menu__header"])}>
+                <div className={classnames(styles["c-home-menu__header-content"])}>
+                  {!isInfoPage && <h2 className={styles["c-home-menu__header-text"]}>Understand the emergency</h2>}
+                  {isInfoPage && (
                     <>
-                      {!isMobile && <h2 className={styles["c-home-menu__info-title"]}>{infoData.title}</h2>}
-                      <p className="u-text-pre-line">{infoData.description}</p>
+                      <button className={styles["c-home-menu__back-button"]} onClick={onBack} aria-label="Back" />
+                      {infoData && isMobile && (
+                        <h2 className={classnames(styles["c-home-menu__header-text"], "u-text-center")}>
+                          {infoData.title}
+                        </h2>
+                      )}
                     </>
                   )}
-                </TabPanel>
+                  {onClose && (
+                    <button className={styles["c-home-menu__close-button"]} onClick={onClose} aria-label="Close menu" />
+                  )}
+                </div>
+                <TabList className={classnames(styles["c-home-menu__tab-list"], "u-padding-top-xs")}>
+                  <Tab
+                    className={classnames(styles["c-home-menu__tab"], "u-margin-right-l")}
+                    data-testid="tab-1"
+                    ref={ref}
+                  >
+                    Climate Alerts
+                  </Tab>
+                  <Tab className={classnames(styles["c-home-menu__tab"], "u-margin-right-l")} data-testid="tab-2">
+                    Data Highlights
+                  </Tab>
+                  <Tab className={styles["c-home-menu__tab"]} data-testid="tab-3">
+                    Advanced Menu
+                  </Tab>
+                  <Tab className="u-display-none" data-testid="tab-4">
+                    Info
+                  </Tab>
+                </TabList>
               </div>
-            </div>
-          </Tabs>
-        </div>
-      </div>
+              <div className={classnames(styles["c-home-menu__content"], "u-padding-none")}>
+                <div className={classnames(styles["c-home-menu__tab-container"])}>
+                  <TabPanel className={styles["c-home-menu__tab-panel"]} data-testid="panel-1">
+                    <HeadlinePanel onForceInfoPage={() => setForceInfoPage(true)} forceInfoPage={forceInfoPage} />
+                  </TabPanel>
+                  <TabPanel className={styles["c-home-menu__tab-panel"]} data-testid="panel-2">
+                    <DataHighlightsPanel />
+                  </TabPanel>
+                  <TabPanel className={styles["c-home-menu__tab-panel"]} data-testid="panel-3">
+                    <DataPanel
+                      currentMode={currentMode}
+                      setCurrentMode={setCurrentMode}
+                      modes={modes}
+                      datasetValue={datasetValue}
+                      setDatasetValue={setDatasetValue}
+                      monitorValue={monitorValue}
+                      heightValue={heightValue}
+                      setHeightValue={setHeightValue}
+                      setMonitorValue={setMonitorValue}
+                      animationValue={animationValue}
+                      animationEnabled={animationEnabled}
+                      setAnimationValue={setAnimationValue}
+                      isMobile={isMobile}
+                      layers={layers}
+                      // TODO Refactor Data panel to pull from redux instead of passing props from parent.
+                    />
+                  </TabPanel>
+                  <TabPanel>
+                    {infoData && (
+                      <>
+                        {!isMobile && <h2 className={styles["c-home-menu__info-title"]}>{infoData.title}</h2>}
+                        <p className="u-text-pre-line">{infoData.description}</p>
+                      </>
+                    )}
+                  </TabPanel>
+                </div>
+              </div>
+            </Tabs>
+          </div>
+        </DialogPanel>
+      )
     );
   }
 );
@@ -180,10 +161,14 @@ Menu.displayName = "Menu";
 
 Menu.propTypes = {
   isMobile: PropTypes.bool.isRequired,
-  isClosing: PropTypes.bool.isRequired,
+  isOpen: PropTypes.bool,
   onClose: PropTypes.func,
   layers: PropTypes.array.isRequired,
   animationEnabled: PropTypes.bool.isRequired
+};
+
+Menu.defaultProps = {
+  isOpen: false
 };
 
 export default Menu;
