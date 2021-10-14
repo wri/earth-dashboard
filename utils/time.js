e876ba72eba31d3cb47a38bd7ce689fa3ce7c56d
Owen Evans <owen@3sidedcube.com>
2021-10-14 16:32:33 +0100

    lint fixes

--- utils/time.js
+++ utils/time.js
@@ -1,20 +1,20 @@
 export const secondsToHms = value => {
-  const sec = parseInt(value, 10); 
-  let hours = Math.floor(sec / 3600); 
-  let minutes = Math.floor((sec - hours * 3600) / 60); 
+  const sec = parseInt(value, 10);
+  let hours = Math.floor(sec / 3600);
+  let minutes = Math.floor((sec - hours * 3600) / 60);
   let seconds = sec - hours * 3600 - minutes * 60;
 
   if (hours > 0 && minutes < 10) {
-    minutes = '0' + minutes;
+    minutes = "0" + minutes;
   }
 
   if (seconds < 10) {
-    seconds = '0' + seconds;
+    seconds = "0" + seconds;
   }
 
   if (hours == 0) {
-    return minutes + ':' + seconds; // Return in MM:SS format
+    return minutes + ":" + seconds; // Return in MM:SS format
   } else {
-    return hours + ':' + minutes + ':' + seconds; // Return in HH:MM:SS format
+    return hours + ":" + minutes + ":" + seconds; // Return in HH:MM:SS format
   }
-}
+};
