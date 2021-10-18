f4b14918b0fca4b93ca7f1d9b26cb110d192e71e
Owen Evans <owen@3sidedcube.com>
2021-10-18 08:40:03 +0100

    breaking news button fixes

--- layout/header/component.js
+++ layout/header/component.js
@@ -50,7 +50,7 @@ const Header = ({ isMegaMenuOpen, setIsMegaMenuOpen, showHeaderTitle }) => {
             className={styles["c-mega-menu-wrapper__bg"]}
             style={{ maxHeight: headerRef.current && headerRef.current.scrollHeight + "px" }}
           >
-            <header className={styles["c-site-header"]} ref={headerRef}>
+            <header className={classnames(styles["c-site-header"], !isMegaMenuOpen && styles["c-site-header--closed"], isMegaMenuOpen && styles["c-site-header--open"])} ref={headerRef}>
               <div className={styles["c-site-header__logo"]}>
                 <LogoLink />
               </div>
