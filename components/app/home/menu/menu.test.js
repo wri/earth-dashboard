f97d7967554c2f7613449b914ab8a1398a6a9785
Owen Evans <owen@3sidedcube.com>
2021-10-20 07:17:12 +0100

    snapshot update

--- components/app/home/menu/menu.test.js
+++ components/app/home/menu/menu.test.js
@@ -21,236 +21,238 @@ test("<Menu /> renders correctly as desktop", async () => {
   <div
     class="c-home-menu-container"
   >
-    <div
-      class="c-home-menu"
-    >
+    <div>
       <div
-        class="c-home-menu__tabs"
-        data-tabs="true"
+        class="c-home-menu"
       >
         <div
-          class="c-home-menu__header"
+          class="c-home-menu__tabs"
+          data-tabs="true"
         >
           <div
-            class="c-home-menu__header-content"
-          >
-            <h2
-              class="c-home-menu__header-text"
-            >
-              Understand the emergency
-            </h2>
-          </div>
-          <ul
-            class="c-home-menu__tab-list u-padding-top-xs"
-            role="tablist"
-          >
-            <li
-              aria-controls="react-tabs-1"
-              aria-disabled="false"
-              aria-selected="true"
-              class="c-home-menu__tab u-margin-right-l react-tabs__tab--selected"
-              data-testid="tab-1"
-              id="react-tabs-0"
-              role="tab"
-              tabindex="0"
-            >
-              Climate Alerts
-            </li>
-            <li
-              aria-controls="react-tabs-3"
-              aria-disabled="false"
-              aria-selected="false"
-              class="c-home-menu__tab u-margin-right-l"
-              data-testid="tab-2"
-              id="react-tabs-2"
-              role="tab"
-            >
-              Data Highlights
-            </li>
-            <li
-              aria-controls="react-tabs-5"
-              aria-disabled="false"
-              aria-selected="false"
-              class="c-home-menu__tab"
-              data-testid="tab-3"
-              id="react-tabs-4"
-              role="tab"
-            >
-              Advanced Menu
-            </li>
-            <li
-              aria-controls="react-tabs-7"
-              aria-disabled="false"
-              aria-selected="false"
-              class="u-display-none"
-              data-testid="tab-4"
-              id="react-tabs-6"
-              role="tab"
-            >
-              Info
-            </li>
-          </ul>
-        </div>
-        <div
-          class="c-home-menu__content u-padding-none"
-        >
-          <div
-            class="c-home-menu__tab-container"
+            class="c-home-menu__header"
           >
             <div
-              aria-labelledby="react-tabs-0"
-              class="c-home-menu__tab-panel react-tabs__tab-panel--selected"
-              data-testid="panel-1"
-              id="react-tabs-1"
-              role="tabpanel"
+              class="c-home-menu__header-content"
             >
-              <p
-                class="c-home-menu__tab-description u-margin-none"
+              <h2
+                class="c-home-menu__header-text"
               >
-                The effects of human-induced climate change can be seen and felt across the planet.
-                <br />
-                Explore the latest alerts from Mongabay below.
-              </p>
-              <div
-                class="c-home-menu__tab-panel-scroll-area"
-              >
-                <button
-                  class="c-headline-card c-home-menu__headline"
-                  data-testid="headline"
-                >
-                  <img
-                    alt=""
-                    class="c-headline-card__image"
-                    role="presentation"
-                    src="http://localhost:9000/gca-earth-hq/NacueP1wLLPka4nSGQVDxp2MDdaHUCJAC324EGqz.jpg"
-                  />
-                  <div>
-                    <h3
-                      class="c-headline-card__title"
-                    >
-                      One of four North Atlantic right whale calves spotted so far this breeding season struck by ship
-                    </h3>
-                    <p
-                      class="c-headline-card__subtitle"
-                    >
-                      15th September 2021
-                    </p>
-                  </div>
-                </button>
-                <button
-                  class="c-headline-card c-home-menu__headline"
-                  data-testid="headline"
-                >
-                  <img
-                    alt=""
-                    class="c-headline-card__image"
-                    role="presentation"
-                    src="http://localhost:9000/gca-earth-hq/temporibus.png"
-                  />
-                  <div>
-                    <h3
-                      class="c-headline-card__title"
-                    >
-                      A ea modi id error qui.
-                    </h3>
-                    <p
-                      class="c-headline-card__subtitle"
-                    >
-                      9th September 2021
-                    </p>
-                  </div>
-                </button>
-                <button
-                  class="c-headline-card c-home-menu__headline"
-                  data-testid="headline"
-                >
-                  <img
-                    alt=""
-                    class="c-headline-card__image"
-                    role="presentation"
-                    src="http://localhost:9000/gca-earth-hq/reiciendis.png"
-                  />
-                  <div>
-                    <h3
-                      class="c-headline-card__title"
-                    >
-                      Praesentium nobis vel necessitatibus.
-                    </h3>
-                    <p
-                      class="c-headline-card__subtitle"
-                    >
-                      9th September 2021
-                    </p>
-                  </div>
-                </button>
-                <button
-                  class="c-headline-card c-home-menu__headline"
-                  data-testid="headline"
-                >
-                  <img
-                    alt=""
-                    class="c-headline-card__image"
-                    role="presentation"
-                    src="http://localhost:9000/gca-earth-hq/et.png"
-                  />
-                  <div>
-                    <h3
-                      class="c-headline-card__title"
-                    >
-                      Minima velit nam sunt.
-                    </h3>
-                    <p
-                      class="c-headline-card__subtitle"
-                    >
-                      9th September 2021
-                    </p>
-                  </div>
-                </button>
-                <button
-                  class="c-headline-card c-home-menu__headline"
-                  data-testid="headline"
-                >
-                  <img
-                    alt=""
-                    class="c-headline-card__image"
-                    role="presentation"
-                    src="http://localhost:9000/gca-earth-hq/dicta.png"
-                  />
-                  <div>
-                    <h3
-                      class="c-headline-card__title"
-                    >
-                      Consequuntur sunt voluptate rerum nemo suscipit et.
-                    </h3>
-                    <p
-                      class="c-headline-card__subtitle"
-                    >
-                      9th September 2021
-                    </p>
-                  </div>
-                </button>
-              </div>
+                Understand the emergency
+              </h2>
             </div>
+            <ul
+              class="c-home-menu__tab-list u-padding-top-xs"
+              role="tablist"
+            >
+              <li
+                aria-controls="react-tabs-1"
+                aria-disabled="false"
+                aria-selected="true"
+                class="c-home-menu__tab u-margin-right-l react-tabs__tab--selected"
+                data-testid="tab-1"
+                id="react-tabs-0"
+                role="tab"
+                tabindex="0"
+              >
+                Climate Alerts
+              </li>
+              <li
+                aria-controls="react-tabs-3"
+                aria-disabled="false"
+                aria-selected="false"
+                class="c-home-menu__tab u-margin-right-l"
+                data-testid="tab-2"
+                id="react-tabs-2"
+                role="tab"
+              >
+                Data Highlights
+              </li>
+              <li
+                aria-controls="react-tabs-5"
+                aria-disabled="false"
+                aria-selected="false"
+                class="c-home-menu__tab"
+                data-testid="tab-3"
+                id="react-tabs-4"
+                role="tab"
+              >
+                Advanced Menu
+              </li>
+              <li
+                aria-controls="react-tabs-7"
+                aria-disabled="false"
+                aria-selected="false"
+                class="u-display-none"
+                data-testid="tab-4"
+                id="react-tabs-6"
+                role="tab"
+              >
+                Info
+              </li>
+            </ul>
+          </div>
+          <div
+            class="c-home-menu__content u-padding-none"
+          >
             <div
-              aria-labelledby="react-tabs-2"
-              class="c-home-menu__tab-panel"
-              data-testid="panel-2"
-              id="react-tabs-3"
-              role="tabpanel"
-            />
-            <div
-              aria-labelledby="react-tabs-4"
-              class="c-home-menu__tab-panel"
-              data-testid="panel-3"
-              id="react-tabs-5"
-              role="tabpanel"
-            />
-            <div
-              aria-labelledby="react-tabs-6"
-              class="react-tabs__tab-panel"
-              id="react-tabs-7"
-              role="tabpanel"
-            />
+              class="c-home-menu__tab-container"
+            >
+              <div
+                aria-labelledby="react-tabs-0"
+                class="c-home-menu__tab-panel react-tabs__tab-panel--selected"
+                data-testid="panel-1"
+                id="react-tabs-1"
+                role="tabpanel"
+              >
+                <p
+                  class="c-home-menu__tab-description u-margin-none"
+                >
+                  The effects of human-induced climate change can be seen and felt across the planet.
+                  <br />
+                  Explore the latest alerts from Mongabay below.
+                </p>
+                <div
+                  class="c-home-menu__tab-panel-scroll-area"
+                >
+                  <button
+                    class="c-headline-card c-home-menu__headline"
+                    data-testid="headline"
+                  >
+                    <img
+                      alt=""
+                      class="c-headline-card__image"
+                      role="presentation"
+                      src="http://localhost:9000/gca-earth-hq/NacueP1wLLPka4nSGQVDxp2MDdaHUCJAC324EGqz.jpg"
+                    />
+                    <div>
+                      <h3
+                        class="c-headline-card__title"
+                      >
+                        One of four North Atlantic right whale calves spotted so far this breeding season struck by ship
+                      </h3>
+                      <p
+                        class="c-headline-card__subtitle"
+                      >
+                        15th September 2021
+                      </p>
+                    </div>
+                  </button>
+                  <button
+                    class="c-headline-card c-home-menu__headline"
+                    data-testid="headline"
+                  >
+                    <img
+                      alt=""
+                      class="c-headline-card__image"
+                      role="presentation"
+                      src="http://localhost:9000/gca-earth-hq/temporibus.png"
+                    />
+                    <div>
+                      <h3
+                        class="c-headline-card__title"
+                      >
+                        A ea modi id error qui.
+                      </h3>
+                      <p
+                        class="c-headline-card__subtitle"
+                      >
+                        9th September 2021
+                      </p>
+                    </div>
+                  </button>
+                  <button
+                    class="c-headline-card c-home-menu__headline"
+                    data-testid="headline"
+                  >
+                    <img
+                      alt=""
+                      class="c-headline-card__image"
+                      role="presentation"
+                      src="http://localhost:9000/gca-earth-hq/reiciendis.png"
+                    />
+                    <div>
+                      <h3
+                        class="c-headline-card__title"
+                      >
+                        Praesentium nobis vel necessitatibus.
+                      </h3>
+                      <p
+                        class="c-headline-card__subtitle"
+                      >
+                        9th September 2021
+                      </p>
+                    </div>
+                  </button>
+                  <button
+                    class="c-headline-card c-home-menu__headline"
+                    data-testid="headline"
+                  >
+                    <img
+                      alt=""
+                      class="c-headline-card__image"
+                      role="presentation"
+                      src="http://localhost:9000/gca-earth-hq/et.png"
+                    />
+                    <div>
+                      <h3
+                        class="c-headline-card__title"
+                      >
+                        Minima velit nam sunt.
+                      </h3>
+                      <p
+                        class="c-headline-card__subtitle"
+                      >
+                        9th September 2021
+                      </p>
+                    </div>
+                  </button>
+                  <button
+                    class="c-headline-card c-home-menu__headline"
+                    data-testid="headline"
+                  >
+                    <img
+                      alt=""
+                      class="c-headline-card__image"
+                      role="presentation"
+                      src="http://localhost:9000/gca-earth-hq/dicta.png"
+                    />
+                    <div>
+                      <h3
+                        class="c-headline-card__title"
+                      >
+                        Consequuntur sunt voluptate rerum nemo suscipit et.
+                      </h3>
+                      <p
+                        class="c-headline-card__subtitle"
+                      >
+                        9th September 2021
+                      </p>
+                    </div>
+                  </button>
+                </div>
+              </div>
+              <div
+                aria-labelledby="react-tabs-2"
+                class="c-home-menu__tab-panel"
+                data-testid="panel-2"
+                id="react-tabs-3"
+                role="tabpanel"
+              />
+              <div
+                aria-labelledby="react-tabs-4"
+                class="c-home-menu__tab-panel"
+                data-testid="panel-3"
+                id="react-tabs-5"
+                role="tabpanel"
+              />
+              <div
+                aria-labelledby="react-tabs-6"
+                class="react-tabs__tab-panel"
+                id="react-tabs-7"
+                role="tabpanel"
+              />
+            </div>
           </div>
         </div>
       </div>
