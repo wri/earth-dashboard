449844cd3cd6b410c6067b00ff0a3cc788527b28
Owen Evans <owen@3sidedcube.com>
2021-10-26 12:40:17 +0100

    mobile fix, lint fix, test fix

--- layout/header/mega-menu/cta/component.js
+++ layout/header/mega-menu/cta/component.js
@@ -32,12 +32,12 @@ const MegaMenuCTA = ({ className, hasarrow, image, label, body, link }) => {
           )}
 
           <Link href={link}>
-            <a className={classnames(styles["c-mega-menu-cta__link"], !hasarrow && "u-visually-hidden-focusable")} aria-labelledby={titleID} aria-describedby={bodyID}>
-              {hasarrow ? (
-                <Image src={LinkIcon} role="presentation" alt="" />
-              ) : (
-                <span>Visit Page</span>
-              )}
+            <a
+              className={classnames(styles["c-mega-menu-cta__link"], !hasarrow && "u-visually-hidden-focusable")}
+              aria-labelledby={titleID}
+              aria-describedby={bodyID}
+            >
+              {hasarrow ? <Image src={LinkIcon} role="presentation" alt="" /> : <span>Visit Page</span>}
             </a>
           </Link>
         </div>
