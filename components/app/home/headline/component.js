313806bed6085c48ddd4c29415bbbd5b2ef97fe7
Owen Evans <owen@3sidedcube.com>
2021-10-28 08:59:53 +0100

    wip - reseting the date using the "now" flag - but not working

--- components/app/home/headline/component.js
+++ components/app/home/headline/component.js
@@ -37,7 +37,7 @@ const Headline = ({
     return () => {
       setIsDatePickerDisabled(false);
       setIsFetchLocationDisabled(false);
-      setDateOfDataShown(new Date().toString());
+      setDateOfDataShown("now");
     };
   }, [setIsDatePickerDisabled]);
 
