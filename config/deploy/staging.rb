5e476d652cc44cf2fc745cca059be8971e0be8b8
Kevin Borrill <kevin@3sidedcube.com>
2022-07-13 12:47:46 +0100

    update branch to test change

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,7 +2,7 @@ server '54.197.134.233', user: 'ubuntu', roles: %w{web app db}, primary: true
 
 set :ssh_options, forward_agent: true
 
-set :branch, 'develop'
+set :branch, 'feature/update-deployments'
 
 set :default_env, {
   'ED_NODE_ENV' => 'production',
