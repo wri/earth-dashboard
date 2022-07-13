10af8af067b91158f7a91940419596a8eb0dc88b
Kevin Borrill <kevin@3sidedcube.com>
2022-07-13 12:24:26 +0100

    Test deploy change title

--- layout/header/header-title/index.js
+++ layout/header/header-title/index.js
@@ -5,7 +5,7 @@ import styles from "./header-title.module.scss";
 const HeaderTitle = ({ className, ...rest }) => {
   return (
     <div className={classnames("u-text-center", className, styles["c-header-title"])} {...rest}>
-      <h1 className={styles["c-header-title__title"]}>This is a planetary emergency</h1>
+      <h1 className={styles["c-header-title__title"]}>This is a planetary emergency - test</h1>
       <p className={styles["c-header-title__subtitle"]}>Explore real-time data</p>
     </div>
   );
