777d1701d27a086fe2ea00716efd0da4a38b2b5e
Mikey Lau <mikey.lau@traininblocks.com>
2022-08-24 21:35:48 +0100

    refactor: Remove unused code

--- layout/app/news/index.js
+++ layout/app/news/index.js
@@ -2,19 +2,6 @@ import NewsTopicLayoutComponent from "./component";
 import { connect } from "react-redux";
 import { setIsMobile } from "slices/common";
 
-// TODO: Temporary bridging design, need to remove later
-const NewsIndex = () => {
-  return (
-    <Layout>
-      <Section gridClassName={styles["c-topics"]} noWrap>
-        {MenuItems.map(link => (
-          <TopicCard key={link.key} {...link} />
-        ))}
-      </Section>
-    </Layout>
-  );
-};
-
 export default connect(state => ({}), {
   setIsMobile
 })(NewsTopicLayoutComponent);
