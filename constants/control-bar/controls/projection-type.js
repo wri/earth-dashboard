0fdfab215c028496c12fbfe75d1cc202b7ed05c1
Mikey Lau <mikey.lau@traininblocks.com>
2022-08-24 20:58:16 +0100

    refactor: Fix and update map control icons

--- constants/control-bar/controls/projection-type.js
+++ constants/control-bar/controls/projection-type.js
@@ -2,8 +2,8 @@ import { setGlobe2d, setGlobe3d, isGlobe2d } from "slices/mapControls";
 
 const projectionTypeConfig = {
   key: "projection-type-control",
-  name: "2D-toggle",
-  activeName: "3D-toggle",
+  name: "2d-toggle",
+  activeName: "3d-toggle",
   isActiveSelector: isGlobe2d,
   getDispatch: isActive => (isActive ? setGlobe3d : setGlobe2d),
   disabled: false,
