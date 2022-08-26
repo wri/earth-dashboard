09a27b97d435098f65fdbcfb0a89e6e5e39af3f4
David Odesola <faithodesola@gmail.com>
2022-08-26 09:44:01 +0100

    Format header section

--- layout/layout/layout-app/component.js
+++ layout/layout/layout-app/component.js
@@ -57,7 +57,14 @@ function LayoutApp(props) {
       <div className={styles["main-banner"]}>
         <div className={styles["left-content"]}>
           <div className={styles["cookie"]}>
-            <img className={styles["cookie-svg"]} src="/static/icons/cookie.svg" role="presentation" alt="" />
+            <img
+              width={32}
+              height={32}
+              className={styles["cookie-svg"]}
+              src="/static/icons/cookie.svg"
+              role="presentation"
+              alt=""
+            />
             <h3 className={styles["cookie-text"]}>COOKIES</h3>
           </div>
           <div className={styles["text"]}>
