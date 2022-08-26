134b881539d50a0a5499bbe10c6901f940515aba
David Odesola <faithodesola@gmail.com>
2022-08-26 10:59:27 +0100

    remove unused field

--- layout/layout/layout-app/component.js
+++ layout/layout/layout-app/component.js
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-import { useState, useEffect, Image } from "react";
+import { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import classnames from "classnames";
 import Modal from "react-modal";
