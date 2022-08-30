08e0bc79e2d1a0d4861bd0ec19b9d50d0fe00d5c
Mikey Lau <mikey@3sidedcube.com>
2022-08-30 16:56:06 +0100

    refactor(settings): Style basemap section on modal

--- components/app/home/settings-menu/basemaps/component.tsx
+++ components/app/home/settings-menu/basemaps/component.tsx
@@ -3,6 +3,7 @@ import RadioImage from "components/form/radio-image";
 import geographyImage from "public/static/images/basemaps/geography-basemap-thumbnail.png";
 import simpleImage from "public/static/images/basemaps/simple-basemap-thumbnail.png";
 import { BasemapType, GlobalSetting } from "slices/globalSettings";
+import styles from "./basemaps.module.scss";
 
 type BasemapsProps = {
   activeOptionId: string;
@@ -20,16 +21,19 @@ const Basemaps = ({ activeOptionId, setSettingById }: BasemapsProps) => {
   };
 
   return (
-    <RadioImage
-      name="basemaps"
-      label="Basemaps"
-      options={[
-        { id: "geography", label: "Geography", image: geographyImage },
-        { id: "simple", label: "Simple", image: simpleImage }
-      ]}
-      activeOptionId={activeOptionId}
-      onChange={handleChange}
-    />
+    <div className={styles["c-basemaps"]}>
+      <RadioImage
+        name="basemaps"
+        label="Basemaps & Data"
+        options={[
+          { id: "geography", label: "Geography", image: geographyImage },
+          { id: "simple", label: "Simple", image: simpleImage }
+        ]}
+        activeOptionId={activeOptionId}
+        onChange={handleChange}
+        className={styles["c-basemaps__selector"]}
+      />
+    </div>
   );
 };
 
