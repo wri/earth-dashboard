83145056172ed0e5f516e7c46e4f8ce5b0c44390
Mikey Lau <mikey@3sidedcube.com>
2022-08-30 14:45:36 +0100

    refactor(settings): Icon header title on modal

--- components/app/home/settings-menu/component.tsx
+++ components/app/home/settings-menu/component.tsx
@@ -5,6 +5,7 @@ import styles from "./settings-menu.module.scss";
 import useDialogPanel from "hooks/useDialogPanel";
 import { ActionCreatorWithoutPayload } from "@reduxjs/toolkit";
 import Preferences from "./preferences";
+import Icon from "components/ui/Icon";
 
 type SettingsMenuProps = {
   isOpen: boolean;
@@ -21,12 +22,15 @@ const SettingsMenu = ({ isOpen, onClose, isMobile }: SettingsMenuProps) => {
       <div className={styles["c-settings-menu-modal"]} aria-labelledby="settingsModalTitle" role="document">
         {/* Settings */}
         <div className={classnames(styles["c-settings-menu-modal__header"], "u-text-center")}>
-          <h2
-            id="settingsModalTitle"
-            className={classnames(styles["c-settings-menu-modal__header__title"], "u-margin-bottom-none")}
-          >
-            Settings
-          </h2>
+          {/* Title */}
+          <div>
+            <Icon name="more" size={30} type="decorative" className={styles["icon"]} />
+            <h2 id="settingsModalTitle" className={classnames(styles["title"], "u-margin-bottom-none")}>
+              More
+            </h2>
+          </div>
+
+          {/* Close button */}
           <IconButton
             name="close"
             aria-label="Close Settings"
