972f162ce4806b472210d9940254d4f324f700c0
Mikey Lau <mikey@3sidedcube.com>
2022-08-30 11:59:01 +0100

    refactor(settings): Style dropdown form fields

--- components/app/home/settings-menu/preferences/component.tsx
+++ components/app/home/settings-menu/preferences/component.tsx
@@ -1,32 +1,37 @@
 import SelectInput from "components/ui/select";
 import { LANGUAGES, UNITS } from "constants/settings";
 import { useState } from "react";
+import styles from "./preferences.module.scss";
 
 const Preferences = () => {
   const [language, setLanguage] = useState("en");
   const [unit, setUnit] = useState("metric");
 
   return (
-    <div>
+    <div className={styles["c-preferences"]}>
       <h3>Preferences</h3>
 
       {/* Languages */}
-      <h4>Language</h4>
-      <SelectInput
-        options={LANGUAGES}
-        value={LANGUAGES.find(option => option.value === language)}
-        onChange={setLanguage}
-        aria-label="Select a language"
-      />
+      <div className={styles["c-preferences__language"]}>
+        <h4>Language</h4>
+        <SelectInput
+          options={LANGUAGES}
+          value={LANGUAGES.find(option => option.value === language)}
+          onChange={setLanguage}
+          aria-label="Select a language"
+        />
+      </div>
 
       {/* Units */}
-      <h4>Units</h4>
-      <SelectInput
-        options={UNITS}
-        value={UNITS.find(option => option.value === unit)}
-        onChange={setUnit}
-        aria-label="Select a unit"
-      />
+      <div className={styles["c-preferences__unit"]}>
+        <h4>Units</h4>
+        <SelectInput
+          options={UNITS}
+          value={UNITS.find(option => option.value === unit)}
+          onChange={setUnit}
+          aria-label="Select a unit"
+        />
+      </div>
     </div>
   );
 };
