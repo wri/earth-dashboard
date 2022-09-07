11b0f221dd9822266ca23c1134ec854ac02d73f7
zayn-m <zain@3sidedcube.com>
2022-09-07 20:16:14 +0100

    fix conflicts

---
+++ components/app/home/headline-footer/index.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import IconButton from "components/ui/icon-button";
+import classnames from "classnames";
+import styles from "./headline-footer.module.scss";
+
+type HeadlineFooterProps = {
+  footerHeading: string;
+  disableBackButton: boolean;
+  disableNextButton: boolean;
+  onTouchStart: () => void;
+  onTouchMove: () => void;
+  onTouchEnd: () => void;
+  navigateHeadline: (action: string) => void;
+};
+
+export default function HeadlineFooter({
+  footerHeading,
+  disableBackButton,
+  disableNextButton,
+  onTouchStart,
+  onTouchMove,
+  onTouchEnd,
+  navigateHeadline
+}: HeadlineFooterProps) {
+  return (
+    <div
+      className={classnames(styles["c-home-menu__footer"])}
+      onTouchStart={onTouchStart}
+      onTouchMove={onTouchMove}
+      onTouchEnd={onTouchEnd}
+    >
+      <IconButton name="arrow-left" medium disabled={disableBackButton} onClick={() => navigateHeadline("back")} />
+      <div className={classnames(styles["c-home-menu__footer-info"])}>
+        <h3>{footerHeading}</h3>
+      </div>
+      <IconButton name="arrow-right" medium disabled={disableNextButton} onClick={() => navigateHeadline("next")} />
+    </div>
+  );
+}
--- components/app/home/menu/component.js
+++ components/app/home/menu/component.js
@@ -6,9 +6,10 @@ import DataIndexPanel from "./panels/data-options";
 import DataLayerPanel from "./panels/data-layer";
 import MenuLayout from "./layout";
 import { fireEvent } from "utils/gtag";
-import { MENU_TAB_CHANGE_EVENT_NAME } from "constants/tag-manager";
+import { MENU_TAB_CHANGE_EVENT_NAME, CLIMATE_ALERT_EVENT_NAME } from "constants/tag-manager";
 import ClimateAlerts from "./panels/climate-alerts";
 import Headline from "../headline";
+import HeadlineFooter from "../headline-footer";
 
 const INFO_PAGE_ID = "InfoPage";
 const EXTREME_EVENTS_PAGE_ID = "ExtremeEventsPage";
@@ -16,6 +17,9 @@ const DATA_LAYER_PAGE_ID = "DataLayerPage";
 
 const INFO_PAGE_HEADLINE = "I'd like to explore";
 const EXTREME_EVENTS_PAGE_HEADLINE = "Extreme events";
+
+const MAX_NUMBER_OF_HEADLINES = 10;
+const MIN_SWIPE_DISTANCE = 50;
 
 const Menu = forwardRef(
   (
@@ -51,9 +55,45 @@ const Menu = forwardRef(
     ref
   ) => {
     const [pageTypeId, setPageTypeId] = useState(INFO_PAGE_ID);
+    const [disableBackButton, setDisableBackButton] = useState(false);
+    const [disableNextButton, setDisableNextButton] = useState(false);
+    const [footerHeading, setFooterHeading] = useState("");
+    const [touchStart, setTouchStart] = useState(null);
+    const [touchEnd, setTouchEnd] = useState(null);
+
+    const mostRecentHeadlines = useMemo(() => {
+      const reversed = [...headlines].reverse();
+      return reversed.slice(0, MAX_NUMBER_OF_HEADLINES);
+    }, [headlines]);
 
     const showExtremeEvents = () => {
       setPageTypeId(EXTREME_EVENTS_PAGE_ID);
+    };
+
+    const getCurrentHeadlineIndex = () => {
+      return currentHeadline ? mostRecentHeadlines.findIndex(headline => headline.id === currentHeadline.id) : null;
+    };
+
+    // Checks current headline position to disable back/next buttons
+    const checkCurrentHeadline = () => {
+      const currentHeadlineIndex = getCurrentHeadlineIndex();
+
+      if (currentHeadline) {
+        const type = currentHeadline.type.replace(/([A-Z])/g, " $1");
+        const typeHeading = type.charAt(0).toUpperCase() + type.slice(1);
+        const text = `${currentHeadlineIndex + 1}/${mostRecentHeadlines.length} ${typeHeading}`;
+        setFooterHeading(text);
+      }
+
+      // For disabling back button
+      if (currentHeadline && currentHeadlineIndex === 0) {
+        setDisableBackButton(true);
+      } else if (currentHeadline && currentHeadlineIndex === mostRecentHeadlines.length - 1) {
+        setDisableNextButton(true);
+      } else {
+        setDisableBackButton(false);
+        setDisableNextButton(false);
+      }
     };
 
     // Handle the headline info panel back button click
@@ -61,6 +101,8 @@ const Menu = forwardRef(
       setCurrentMode(undefined);
       setHeadlines([]);
       setPageTypeId(INFO_PAGE_ID);
+      setDisableBackButton(false);
+      setDisableNextButton(false);
     };
 
     const clearHeadline = () => {
@@ -73,9 +115,45 @@ const Menu = forwardRef(
       setPageTypeId(DATA_LAYER_PAGE_ID);
     };
 
+    const navigateHeadline = action => {
+      const headlineIndex = getCurrentHeadlineIndex();
+      let headline = null;
+
+      if (action === "back") {
+        headline = mostRecentHeadlines[headlineIndex - 1];
+        setCurrentHeadline(headline);
+        fireEvent(CLIMATE_ALERT_EVENT_NAME, headline.attributes?.title);
+      } else {
+        headline = mostRecentHeadlines[headlineIndex + 1];
+        setCurrentHeadline(headline);
+        fireEvent(CLIMATE_ALERT_EVENT_NAME, headline.attributes?.title);
+      }
+    };
+
+    const onTouchStart = e => {
+      setTouchEnd(null);
+      setTouchStart(e.targetTouches[0].clientX);
+    };
+
+    const onTouchMove = e => setTouchEnd(e.targetTouches[0].clientX);
+
+    const onTouchEnd = () => {
+      if (!touchStart || !touchEnd) return;
+      const distance = touchStart - touchEnd;
+      const isRightSwipe = distance > MIN_SWIPE_DISTANCE;
+      const isLeftSwipe = distance < -MIN_SWIPE_DISTANCE;
+
+      if (isLeftSwipe) navigateHeadline("back");
+      else navigateHeadline("next");
+    };
+
     useEffect(() => {
       fireEvent(MENU_TAB_CHANGE_EVENT_NAME, INFO_PAGE_HEADLINE);
     }, [setCurrentHeadline]);
+
+    useEffect(() => {
+      checkCurrentHeadline();
+    }, [currentHeadline]);
 
     return (
       <div
@@ -89,6 +167,15 @@ const Menu = forwardRef(
             setDialogHeight={setDialogHeight}
           >
             <Headline headline={currentHeadline} />
+            <HeadlineFooter
+              footerHeading={footerHeading}
+              disableBackButton={disableBackButton}
+              disableNextButton={disableNextButton}
+              onTouchStart={onTouchStart}
+              onTouchMove={onTouchMove}
+              onTouchEnd={onTouchEnd}
+              navigateHeadline={navigateHeadline}
+            />
           </MenuLayout>
         )}
         {!currentHeadline && pageTypeId == INFO_PAGE_ID && (
