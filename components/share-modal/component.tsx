c671a7a7a422423f03c9743968e414d38af13a0a
Dominik Alkhovik <dominik@3sidedcube.com>
2022-09-07 12:05:39 +0100

    start adding share functionality

--- components/share-modal/component.tsx
+++ components/share-modal/component.tsx
@@ -1,4 +1,4 @@
-import React, { SetStateAction } from "react";
+import React, { SetStateAction, useState } from "react";
 import DialogPanel from "components/app/home/dialog-panel";
 import classnames from "classnames";
 import styles from "./share-modal.module.scss";
@@ -14,7 +14,35 @@ type ShareModalProps = {
 };
 
 const ShareModal = ({ onClose, currentHeadline }: ShareModalProps) => {
-  console.log(currentHeadline);
+  const [copiedLinkTimeout, setCopiedLinkTimeout] = useState<boolean>(false);
+
+  // Opens facebook share page
+  const handleFaceBookPress = () => {
+    const link = window.location.href;
+    window.open(`https://www.facebook.com/sharer/sharer.php?u=${link}`, "_blank");
+  };
+
+  // Copies the link to the clipboard
+  const handleCopyPress = () => {
+    const link = window.location.href;
+    navigator.clipboard.writeText(link);
+    setCopiedLinkTimeout(true);
+    setTimeout(() => setCopiedLinkTimeout(false), 1000);
+  };
+
+  const handleInstagramPress = () => {
+    console.log("instagram");
+  };
+
+  // Opens native share method
+  const handleMorePress = async () => {
+    const link = window.location.href;
+    try {
+      await navigator.share({ title: "Earth HQ", url: link });
+    } catch (err) {
+      console.log("Error: " + err);
+    }
+  };
 
   return (
     <DialogPanel onClose={onClose} isMobile shouldAnimate={false}>
@@ -26,23 +54,23 @@ const ShareModal = ({ onClose, currentHeadline }: ShareModalProps) => {
           </h2>
 
           {/* Close button */}
-          <IconButton
-            name="close"
-            size={12}
-            aria-label="Close Settings"
-            // className={styles["c-share-modal__close"]}
-            onClick={() => onClose(false)}
-            small
-          />
+          <IconButton name="close" size={12} aria-label="Close Settings" onClick={() => onClose(false)} small />
         </div>
         {/* Body */}
         <div className={classnames(styles["c-share-modal__body"])}>
           <div className={classnames(styles["scroll"])}>
             {currentHeadline ? <HeadlineCard currentHeadline={currentHeadline} /> : <GlobeCard />}
-            <ShareRow text="Share Via FaceBook" icon="facebook" onClick={() => console.log("facebook")} />
-            <ShareRow text="Share Via Instagram" icon="instagram" onClick={() => console.log("insta")} />
-            <ShareRow text="Copy Link" icon="copy-link" onClick={() => console.log("copy")} />
-            <ShareRow text="More Options" icon="more" onClick={() => console.log("more")} />
+            <ShareRow text="Share Via FaceBook" icon="facebook" onClick={handleFaceBookPress} />
+            <ShareRow text="Share Via Instagram" icon="instagram" onClick={handleInstagramPress} />
+            <ShareRow
+              text={copiedLinkTimeout ? "Copied Link ..." : "Copy Link"}
+              icon="copy-link"
+              onClick={handleCopyPress}
+            />
+            {
+              // @ts-expect-error
+              navigator.share && <ShareRow text={"More Options"} icon="more" onClick={handleMorePress} />
+            }
           </div>
         </div>
       </div>
