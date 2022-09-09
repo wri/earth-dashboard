cd18196cc26e98b207caa3712fb850dde2b3cec6
David Odesola <david.odesola@3sidedcube.com>
2022-09-09 16:37:52 +0100

    linked cookie toggle to localstorage

--- components/app/home/settings-menu/cookie-preferences/index.tsx
+++ components/app/home/settings-menu/cookie-preferences/index.tsx
@@ -1,8 +1,17 @@
 import FormSwitch from "components/form/switch";
+import { ANALYTICS_ACCEPTED, COOKIES_ACCEPTED } from "layout/layout/layout-app/constants";
+import { useEffect, useState } from "react";
 import styles from "./cookie-preferences.module.scss";
 
 /** Contains all preference global settings. */
 const CookiePreferences = () => {
+  const [analytics, setAnalytics] = useState<boolean>(true);
+  const [cookies, setCookies] = useState<boolean>(true);
+
+  useEffect(() => {
+    setAnalytics(localStorage.getItem(ANALYTICS_ACCEPTED) === "false" ? false : true);
+    setCookies(localStorage.getItem(COOKIES_ACCEPTED) === "false" ? false : true);
+  }, []);
   return (
     <div className={styles["c-preferences"]}>
       <h3>Preferences</h3>
@@ -10,16 +19,22 @@ const CookiePreferences = () => {
       {/* Analytics toggle */}
       <FormSwitch
         label="Analytics"
-        onChange={() => console.log("")}
-        isActive={true}
+        onChange={e => {
+          setAnalytics(e);
+          localStorage.setItem(ANALYTICS_ACCEPTED, `${e}`);
+        }}
+        isActive={analytics}
         className={styles["c-preferences__toggle--analytics"]}
       />
 
       {/* Cookies toggle */}
       <FormSwitch
         label="Cookies"
-        onChange={() => console.log("")}
-        isActive={true}
+        onChange={e => {
+          setCookies(e);
+          localStorage.setItem(COOKIES_ACCEPTED, `${e}`);
+        }}
+        isActive={cookies}
         className={styles["c-preferences__toggle--cookies"]}
       />
     </div>
