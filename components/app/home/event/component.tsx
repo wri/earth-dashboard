1bf999b654083749ee947e85aa4fdadc6b2a27a9
Mark Somerfield <mark@3sidedcube.com>
2022-09-13 15:48:20 +0100

    GCAG-175: Restore event focus when selecting headline

--- components/app/home/event/component.tsx
+++ components/app/home/event/component.tsx
@@ -11,16 +11,30 @@ import NormalScale from "components/app/home/normal-scale/component";
 import moment from "moment";
 import Image from "next/image";
 import CtaButton from "components/ui/cta-button";
+import { ActionCreatorWithPayload } from "@reduxjs/toolkit";
 
 type DataLayerOverviewProps = {
   currentMode?: Mode;
   headline: HeadlineType;
+  setIsDatePickerDisabled: ActionCreatorWithPayload<boolean, string>;
+  setCurrentLocation: ActionCreatorWithPayload<number[], string>;
+  setCurrentScale: ActionCreatorWithPayload<number, string>;
+  setCurrentScaleBy: ActionCreatorWithPayload<number, string>;
+  setDateOfDataShown: ActionCreatorWithPayload<string, string>;
 };
 
 const WHAT_IS_HAPPENING_ICON = "/static/icons/question.svg";
 const SHARE_ICON = "/static/icons/together.svg";
 
-const ExtremeEvent = ({ headline, currentMode }: DataLayerOverviewProps) => {
+const ExtremeEvent = ({
+  headline,
+  currentMode,
+  setIsDatePickerDisabled,
+  setCurrentLocation,
+  setCurrentScale,
+  setCurrentScaleBy,
+  setDateOfDataShown
+}: DataLayerOverviewProps) => {
   const containerRef = useRef<HTMLDivElement>(null);
   const [scrollPosition, setScrollPosition] = useState(0);
   const [containerHeight, setContainerHeight] = useState(0);
@@ -28,6 +42,18 @@ const ExtremeEvent = ({ headline, currentMode }: DataLayerOverviewProps) => {
   useEffect(() => {
     setContainerHeight(containerRef?.current?.offsetHeight ?? 0);
   }, [containerRef?.current?.clientHeight, headline?.id]);
+
+  useEffect(() => {
+    if (headline?.attributes.location) {
+      setCurrentLocation([headline?.attributes.location.lat, headline?.attributes.location.lng]);
+      setCurrentScale(headline.attributes.zoom_level);
+      setCurrentScaleBy(1);
+    }
+
+    if (headline?.attributes.climate_alert_date) {
+      setDateOfDataShown(new Date(headline?.attributes.climate_alert_date).toString());
+    }
+  }, [headline, setCurrentLocation, setCurrentScale, setCurrentScaleBy, setDateOfDataShown]);
 
   const how_to_help_content = currentMode?.attributes?.how_to_help_content;
 
