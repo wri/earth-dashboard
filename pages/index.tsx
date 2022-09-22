c24baa7500d3cbe714285a5522af5f0f8d7797c9
Dominik Alkhovik <dominik@3sidedcube.com>
2022-09-22 16:05:16 +0100

    fetch headline data before render

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,24 @@
 import LayoutHome from "layout/app/home";
 import { connect } from "react-redux";
+import { fetchClimateAlertById } from "services/gca";
 import { Headline } from "slices/headlines";
 import { RootState } from "store/types";
 
 type HomePageProps = {
+  initCurrentHeadline: Headline | undefined;
   currentHeadline: Headline | undefined;
 };
 
-const HomePage = ({ currentHeadline }: HomePageProps) => {
+const HomePage = ({ initCurrentHeadline, currentHeadline }: HomePageProps) => {
   const title = currentHeadline
     ? currentHeadline.attributes.title
+    : initCurrentHeadline
+    ? initCurrentHeadline.attributes.title
     : "The Science is in. This is not a drill. It's a Planetary Emergency";
   const thumbnail = currentHeadline
     ? currentHeadline.attributes.thumbnail_image
+    : initCurrentHeadline
+    ? initCurrentHeadline.attributes.thumbnail_image
     : "https://raw.githubusercontent.com/wri/earth-dashboard/main/public/static/images/share/thumbnails/homepage.jpg";
 
   return (
@@ -22,6 +28,24 @@ const HomePage = ({ currentHeadline }: HomePageProps) => {
       description="Explore the evolving condition of the global commons, the source of pressures, and actions to protect, manage and restore them."
     />
   );
+};
+
+HomePage.getInitialProps = async ({ query }: { query: any }) => {
+  if (!query.headline) return { initCurrentHeadline: undefined };
+
+  try {
+    const resp = await fetchClimateAlertById(query.headline);
+    // @ts-expect-error
+    if (resp.data) {
+      // @ts-expect-error
+      const initCurrentHeadline = resp.data.data;
+      return { initCurrentHeadline };
+    }
+  } catch (err) {
+    console.log("Error fetching headlines", err);
+  }
+
+  return { initCurrentHeadline: undefined };
 };
 
 export default connect((state: RootState) => ({
