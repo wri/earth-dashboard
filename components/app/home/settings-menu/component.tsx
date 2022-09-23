4de6c529321d6f7cffb0613b67fced4d5ed7b4ec
Kevin Borrill <kevin@3sidedcube.com>
2022-09-23 16:14:34 +0100

    Update cookie toggle and copy

--- components/app/home/settings-menu/component.tsx
+++ components/app/home/settings-menu/component.tsx
@@ -100,11 +100,76 @@ const SettingsMenu = ({
             {isCookieOpen ? (
               <div className={styles["main"]}>
                 <div className={styles["c-settings-menu-modal__cookie"]}>
-                  <h3 className={styles["c-settings-menu-modal__cookie__text"]}>About</h3>
+                  <h3 className={styles["c-settings-menu-modal__cookie__text"]}>Cookies</h3>
                   <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales finibus tristique. Lorem
-                    ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sodales finibus tristique. Curabitur
-                    malesuada enim neque, at aliquet ligula dictum in. Duis non malesuada nunc.
+                    We use cookies along with Analytical tools (Google analytics, Crazy Egg and Hotjar) to measure how
+                    you use the website so we can improve it based on user needs. Google Analytics sets cookies that
+                    store anonymised information about how you got to the site, the pages you visit and how long you
+                    spend on each page and what you click on while you are visiting the site.
+                  </p>
+                  <p>You can change your cookie preferences by clicking the toggle below.</p>
+                  <p>
+                    Alternatively, most web browsers allow some control of most cookies through the browser settings. To
+                    find out more about cookies, including how to see what cookies have been set, visit
+                    <a href="http://www.aboutcookies.org/" target="_blank" rel="noreferrer noopenner">
+                      {" "}
+                      www.aboutcookies.org
+                    </a>{" "}
+                    or
+                    <a href="http://www.allaboutcookies.org/" target="_blank" rel="noreferrer noopenner">
+                      {" "}
+                      www.allaboutcookies.org
+                    </a>
+                    .
+                  </p>
+                  <p>Find out how to manage cookies on popular browsers:</p>
+                  <ul>
+                    <li>
+                      <a
+                        href="https://support.google.com/accounts/answer/61416?co=GENIE.Platform%3DDesktop&amp;hl=en"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        Google Chrome
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="https://privacy.microsoft.com/en-us/windows-10-microsoft-edge-and-privacy"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        Microsoft Edge
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="https://support.mozilla.org/en-US/kb/enable-and-disable-cookies-website-preferences"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        Mozilla Firefox
+                      </a>
+                    </li>
+                    <li>
+                      <a href="https://www.opera.com/help/tutorials/security/privacy/" target="_blank" rel="noreferrer">
+                        Opera
+                      </a>
+                    </li>
+                    <li>
+                      <a href="https://support.apple.com/en-gb/safari" target="_blank" rel="noreferrer">
+                        Apple Safari
+                      </a>
+                    </li>
+                  </ul>
+                  <p>To find information relating to other browsers, visit the browser developer&apos;s website.</p>
+                  <p>
+                    To opt out of being tracked by Google Analytics across all websites, visit
+                    <a href="http://tools.google.com/dlpage/gaoptout" target="_blank" rel="noreferrer noopenner">
+                      {" "}
+                      http://tools.google.com/dlpage/gaoptout
+                    </a>
+                    .
                   </p>
                 </div>
                 <CookiePreferences />
--- components/app/home/settings-menu/cookie-preferences/index.tsx
+++ components/app/home/settings-menu/cookie-preferences/index.tsx
@@ -1,5 +1,5 @@
 import FormSwitch from "components/form/switch";
-import { ANALYTICS_ACCEPTED, COOKIES_ACCEPTED } from "layout/layout/layout-app/constants";
+import { ANALYTICS_ACCEPTED } from "layout/layout/layout-app/constants";
 import { useEffect, useState } from "react";
 import styles from "./cookie-preferences.module.scss";
 
@@ -10,7 +10,6 @@ const CookiePreferences = () => {
 
   useEffect(() => {
     setAnalytics(localStorage.getItem(ANALYTICS_ACCEPTED) === "false" ? false : true);
-    setCookies(localStorage.getItem(COOKIES_ACCEPTED) === "false" ? false : true);
   }, []);
   return (
     <div className={styles["c-preferences"]}>
@@ -18,24 +17,14 @@ const CookiePreferences = () => {
 
       {/* Analytics toggle */}
       <FormSwitch
-        label="Analytics"
+        label="Cookies"
         onChange={e => {
           setAnalytics(e);
           localStorage.setItem(ANALYTICS_ACCEPTED, `${e}`);
+          window.dispatchEvent(new Event("storage"));
         }}
         isActive={analytics}
         className={styles["c-preferences__toggle--analytics"]}
-      />
-
-      {/* Cookies toggle */}
-      <FormSwitch
-        label="Cookies"
-        onChange={e => {
-          setCookies(e);
-          localStorage.setItem(COOKIES_ACCEPTED, `${e}`);
-        }}
-        isActive={cookies}
-        className={styles["c-preferences__toggle--cookies"]}
       />
     </div>
   );
