161b17178252861ae99d657deddbd00e23dd3aac
Mikey Lau <mikey@3sidedcube.com>
2022-10-07 17:02:55 +0100

    refactor: 404 text container layout

--- pages/404.tsx
+++ pages/404.tsx
@@ -4,8 +4,10 @@ import styles from "layout/app/404/404.module.scss";
 const PageNotFound = () => {
   return (
     <Layout title="404" className={styles["c-page-404"]}>
-      <h1>404</h1>
-      <p>Ooops, that page could no longer be found. Head back to Earth HQ to explore the latest extreme events.</p>
+      <div className={styles["container"]}>
+        <h1>404</h1>
+        <p>Ooops, that page could no longer be found. Head back to Earth HQ to explore the latest extreme events.</p>
+      </div>
     </Layout>
   );
 };
