78528a234c506c5dda644369bae79bf174f6d241
Mikey Lau <mikey@3sidedcube.com>
2022-10-14 00:23:11 +0100

    refactor(menu): Copy change

--- components/app/home/event-prompt/component.tsx
+++ components/app/home/event-prompt/component.tsx
@@ -1,9 +1,18 @@
 import styles from "./event-prompt.module.scss";
 
-const EventPrompt = () => {
+type EventPromptProps = {
+  isMobile?: boolean;
+};
+
+/** Shows information on how to use the globe. */
+const EventPrompt = ({ isMobile }: EventPromptProps) => {
   return (
     <div className={styles["event-prompt-container"]}>
-      <p>Select a point on the globe to view the latest Extreme Events from Mongabay.</p>
+      {isMobile ? (
+        <p>Swipe left or select a point on the globe to view the latest extreme events from Mongabay.</p>
+      ) : (
+        <p>Select a point on the globe to view the latest Extreme Events from Mongabay.</p>
+      )}
     </div>
   );
 };
