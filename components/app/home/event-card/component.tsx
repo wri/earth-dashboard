3b3ddcc566d17b1a7ef169553918040ca7cc2c75
Mikey Lau <mikey@3sidedcube.com>
2022-10-26 14:56:36 +0100

    fix: Pretty format date on share modal

--- components/app/home/event-card/component.tsx
+++ components/app/home/event-card/component.tsx
@@ -1,7 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import classnames from "classnames";
 import styles from "./event-card.module.scss";
-import moment from "moment";
 import Image from "next/image";
 import { Headline } from "slices/headlines";
 import CtaButton from "components/ui/cta-button";
