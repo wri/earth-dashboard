e877c9698f5fa70dbb2236afab8bd28c44a96668
Mikey Lau <mikey@3sidedcube.com>
2022-10-26 16:04:37 +0100

    fix: Use valid thumbnail

--- pages/explore.tsx
+++ pages/explore.tsx
@@ -11,7 +11,7 @@ const ExplorePage = () => {
   return (
     <LayoutHome
       title="The Science is in. This is not a drill. It's a Planetary Emergency"
-      thumbnail="https://raw.githubusercontent.com/wri/earth-dashboard/main/public/static/images/share/thumbnails/ExplorePage.jpg"
+      thumbnail="https://raw.githubusercontent.com/wri/earth-dashboard/main/public/static/images/share/thumbnails/homepage.jpg"
       description="Explore the evolving condition of the global commons, the source of pressures, and actions to protect, manage and restore them."
     />
   );
