0399afb646553dfe80f2bb51126ee027bc1f672c
Kevin Borrill <kevin@3sidedcube.com>
2023-01-11 17:00:31 +0000

    Restructure news page (split between recent and the rest)

--- layout/app/news/component.tsx
+++ layout/app/news/component.tsx
@@ -1,5 +1,5 @@
 import classnames from "classnames";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import useMongabayPosts from "hooks/useMongabayPosts";
 import Layout from "layout/layout/layout-app";
 import AnchorCTA from "components/ui/anchor-cta";
@@ -12,7 +12,7 @@ import newsStyles from "./news.module.scss";
 import newsArticleStyles from "components/news-article/news-article.module.scss";
 import IconButton from "components/ui/icon-button";
 import Section from "./section";
-import { BG_GALAXY } from "constants/section-colours";
+import { BG_GALAXY, BG_SPACE } from "constants/section-colours";
 import SearchBar from "./search-bar";
 import SearchEmptyState from "./search-empty-state";
 import NewsArticleSkeleton from "components/news-article/news-article-skeleton";
@@ -32,6 +32,23 @@ const NewsSearchLayout = () => {
     topic,
     search
   });
+
+  const { mostRecent, moreNews } = useMemo(() => {
+    if (posts.length <= 3) {
+      return {
+        mostRecent: posts || [],
+        moreNews: []
+      };
+    }
+
+    const moreNews = [...posts];
+    const mostRecent = moreNews.splice(0, 3);
+
+    return {
+      mostRecent,
+      moreNews
+    };
+  }, [posts]);
 
   const hasSearchResults = posts && posts.length > 0;
 
@@ -57,8 +74,47 @@ const NewsSearchLayout = () => {
         />
       </div>
 
-      {/* Search results */}
       <Section
+        gridClassName={classnames(newsArticleStyles["c-page-section-grid-news-articles"], {
+          [newsArticleStyles["no-top-margin"]]: !hasSearchResults
+        })}
+        bgColour={BG_SPACE}
+        title="Most Recent"
+        noWrap
+      >
+        {isLoading
+          ? [...Array(3)].map(key => <NewsArticleSkeleton key={`skeleton-${key}`} />)
+          : mostRecent.map(({ key, ...articleProps }) => <NewsArticle key={key} {...articleProps} />)}
+      </Section>
+      <Section
+        gridClassName={classnames(newsArticleStyles["c-page-section-grid-news-articles"], {
+          [newsArticleStyles["no-top-margin"]]: !hasSearchResults
+        })}
+        bgColour={BG_GALAXY}
+        title="More News"
+        noWrap
+      >
+        {isLoading
+          ? [...Array(6)].map(key => <NewsArticleSkeleton key={`skeleton-${key}`} />)
+          : moreNews.map(({ key, ...articleProps }) => <NewsArticle key={key} {...articleProps} />)}
+
+        {canFetchMore && (
+          <div className={newsArticleStyles["c-page-section-grid-news-articles__load-more"]}>
+            <AnchorCTA
+              className={classnames(
+                newsArticleStyles["c-page-section-grid-news-articles__load-more__btn"],
+                styles["c-page-search__load-more__btn"]
+              )}
+              onClick={() => fetchMore(LOAD_MORE_LIMIT)}
+            >
+              {isFetchingMore ? "Loading..." : "Load More "}
+            </AnchorCTA>
+          </div>
+        )}
+      </Section>
+
+      {/* Search results */}
+      {/* <Section
         gridClassName={classnames(newsArticleStyles["c-page-section-grid-news-articles"], {
           [newsArticleStyles["no-top-margin"]]: !hasSearchResults
         })}
@@ -70,7 +126,7 @@ const NewsSearchLayout = () => {
           : {})}
       >
         {isLoading ? (
-          [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map(key => <NewsArticleSkeleton key={`skeleton-${key}`} />)
+          [...Array(9)].map(key => <NewsArticleSkeleton key={`skeleton-${key}`} />)
         ) : !posts || !posts.length ? (
           <SearchEmptyState />
         ) : (
@@ -78,22 +134,9 @@ const NewsSearchLayout = () => {
             {posts.map(({ key, ...articleProps }) => (
               <NewsArticle key={key} {...articleProps} />
             ))}
-            {canFetchMore && (
-              <div className={newsArticleStyles["c-page-section-grid-news-articles__load-more"]}>
-                <AnchorCTA
-                  className={classnames(
-                    newsArticleStyles["c-page-section-grid-news-articles__load-more__btn"],
-                    styles["c-page-search__load-more__btn"]
-                  )}
-                  onClick={() => fetchMore(LOAD_MORE_LIMIT)}
-                >
-                  {isFetchingMore ? "Loading..." : "Load More "}
-                </AnchorCTA>
-              </div>
-            )}
           </>
         )}
-      </Section>
+      </Section> */}
 
       {hasSearchResults && (
         // @ts-expect-error
