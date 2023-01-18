9176bca0f5c7e1e75ba8cb5b76226a68cde4b2c8
owenjs <owen.evans115@gmail.com>
2023-01-18 14:18:30 +0000

    news_tab event removed

--- pages/news/index.tsx
+++ pages/news/index.tsx
@@ -1,14 +1,6 @@
-import { Desktop, MediaContextProvider, Mobile } from "utils/responsive";
 import NewsLayout from "layout/app/news";
-import { useEffect } from "react";
-import { fireEvent } from "utils/gtag";
-import { PAGE_VIEW } from "constants/tag-manager";
 
 const NewsPage = () => {
-  useEffect(() => {
-    fireEvent(PAGE_VIEW, "news_tab");
-  }, []);
-
   return <NewsLayout />;
 };
 
